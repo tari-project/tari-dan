4e9d3fa201b15d9e704799ecf97e26b0843729a4
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2022-08-29 20:22:39 +0100

    initial version of the transaction builder

--- dan_layer/template_macros/src/lib.rs
+++ dan_layer/template_macros/src/lib.rs
@@ -21,6 +21,7 @@
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 mod template;
+mod transaction;
 
 use proc_macro::TokenStream;
 
---
+++ dan_layer/template_macros/src/transaction/builder.rs
@@ -0,0 +1,112 @@
+// TODO: remove this
+#![allow(dead_code)]
+
+pub struct TransactionBuilder {
+    instructions: Vec<Instruction>,
+}
+
+impl TransactionBuilder {
+    pub fn new() -> Self {
+        // TODO: pass the runtime as an arg
+        Self {
+            instructions: vec![],
+        }
+    }
+
+    pub fn add_instruction(&mut self, instruction: Instruction) {
+        self.instructions.push(instruction);
+    }
+
+    // TODO: add "build" method (serialize, sign, etc)
+    // TODO: add "run" method (to run in an engine)
+}
+
+#[derive(Debug, Clone)]
+pub enum Instruction {
+    CallFunction {
+        package_address: String,
+        template: String,
+        function: String,
+        proofs: Vec<String>, // variables with the badge_proofs
+        args: Vec<Arg>,
+        return_variables: Vec<VariableName>,
+    },
+    CallMethod {
+        package_address: String,
+        component_address: String,
+        method: String,
+        proofs: Vec<String>, // variables with the badge_proofs
+        args: Vec<Arg>,
+        return_variables: Vec<VariableName>,
+    },
+    BucketSplit {
+        input: VariableName,
+        amount: u32, // TODO: use an amount type
+        output_amount: VariableName, // name of the new variable with the specified amount
+        output_change: VariableName, // name of the new variable that will hold the change
+    },
+    BucketJoin {
+        inputs: Vec<VariableName>, // names of all the bucket variables to join
+        output: VariableName, // name of the new output variable
+    },
+    GenerateBadgeProof {
+        input: VariableName,  // must be a bucket variable
+        output: VariableName,
+    },
+    AssertEq {
+        input_a: Arg,
+        input_b: Arg,
+    },
+    AssertNe {
+        input_a: Arg,
+        input_b: Arg,
+    },
+    AssertGt {
+        input_a: Arg,
+        input_b: Arg,
+    },
+    AssertLt {
+        input_a: Arg,
+        input_b: Arg,
+    },
+}
+
+type VariableName = String;
+
+#[derive(Debug, Clone)]
+pub enum Arg {
+    Literal(Value),
+    Variable(VariableName),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Value {
+    // Basic values
+    Unit,
+    Bool(bool),
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
+    I128(i128),
+    U8(u8),
+    U16(u16),
+    U32(u32),
+    U64(u64),
+    U128(u128),
+    String(String),
+
+    // Complex values
+    Tuple(Vec<Value>),
+    ComponentAddress(String), // TODO: address type 
+    Bucket(String), // TODO: resource address
+    Proof(String), // TODO: resource addess
+}
+
+
+
+
+
+
+
+
