753e17d953e71d2ef384f1333541fdba7089418a
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2022-10-11 16:17:34 -0600

    "hello world" cucumber-rs example

---
+++ applications/tari_validator_node/tests/example.rs
@@ -0,0 +1,59 @@
+use std::convert::Infallible;
+
+use async_trait::async_trait;
+use cucumber::{given, then, when, WorldInit};
+
+// These `Cat` definitions would normally be inside your project's code,
+// not test code, but we create them here for the show case.
+#[derive(Debug, Default)]
+struct Cat {
+    pub hungry: bool,
+}
+
+impl Cat {
+    fn feed(&mut self) {
+        self.hungry = false;
+    }
+}
+
+// `World` is your shared, likely mutable state.
+// Cucumber constructs it via `Default::default()` for each scenario.
+#[derive(Debug, Default, WorldInit)]
+pub struct AnimalWorld {
+    cat: Cat,
+}
+
+#[async_trait(?Send)]
+impl cucumber::World for AnimalWorld {
+    type Error = Infallible;
+
+    async fn new() -> Result<Self, Self::Error> {
+        Ok(Self {
+            cat: Cat { hungry: false },
+        })
+    }
+}
+
+// Steps are defined with `given`, `when` and `then` attributes.
+#[given("a hungry cat")]
+fn hungry_cat(world: &mut AnimalWorld) {
+    world.cat.hungry = true;
+}
+
+#[when("I feed the cat")]
+fn feed_cat(world: &mut AnimalWorld) {
+    world.cat.feed();
+}
+
+#[then("the cat is not hungry")]
+fn cat_is_fed(world: &mut AnimalWorld) {
+    assert!(!world.cat.hungry);
+}
+
+// This runs before everything else, so you can setup things here.
+fn main() {
+    // You may choose any executor you like (`tokio`, `async-std`, etc.).
+    // You may even have an `async` main, it doesn't matter. The point is that
+    // Cucumber is composable. :)
+    futures::executor::block_on(AnimalWorld::run("tests/features/book"));
+}
