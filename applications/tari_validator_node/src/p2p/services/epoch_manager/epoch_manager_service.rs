cde08b5311d0bd41ac0003b84e528ebee48358a8
stringhandler <mikethetike@tari.com>
2022-10-13 10:36:22 +0200

    fix: proto conversions (#87)
    fix missing proto conversions

--- applications/tari_validator_node/src/p2p/services/epoch_manager/epoch_manager_service.rs
+++ applications/tari_validator_node/src/p2p/services/epoch_manager/epoch_manager_service.rs
@@ -112,6 +112,9 @@ impl EpochManagerService {
     }
 
     pub async fn run(&mut self, mut shutdown: ShutdownSignal) -> Result<(), EpochManagerError> {
+        // first, load initial state
+        self.inner.load_initial_state().await?;
+
         loop {
             tokio::select! {
                 Some((req, reply)) = self.rx_request.recv() => {
