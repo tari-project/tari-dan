b64cf187704f9e2b33922d309d88cd7184854864
Brian Pearce <brianp@users.noreply.github.com>
2022-10-17 15:13:04 +0200

    feat: vn re-registers each epoch (#95)
    Description
    ---
    - Removes auto registration on startup
    - Adds auto registration when config option `auto_register` is set to true and the epoch changes
    
    Caveats:
    - If auto register is set it'll register on the next epoch change regardless of if it's ever been registered before. VN doesn't currently track its own registered state.
    - ~~The current scanning for epoch change is done on a 2-minute interval which is likely a bad interval type to use. We should probably use the block height, which is being scanned more frequently anyway.~~ The current scanning happens at the same interval as the epoch manager scans the base layer. Might be good to set up a channel to pub/sub epoch changes.
    
    Motivation and Context
    ---
    To keep the VN registered as the epochs change.
    
    How Has This Been Tested?
    ---
    Manually

--- applications/tari_validator_node/src/bootstrap.rs
+++ applications/tari_validator_node/src/bootstrap.rs
@@ -31,6 +31,7 @@ use tari_p2p::initialization::spawn_comms_using_transport;
 use tari_shutdown::ShutdownSignal;
 
 use crate::{
+    auto_registration,
     base_layer_scanner,
     comms,
     grpc::services::base_node_client::GrpcBaseNodeClient,
@@ -142,7 +143,7 @@ pub async fn spawn_services(
         payload_processor,
         rx_consensus_message,
         rx_vote_message,
-        shutdown,
+        shutdown.clone(),
     )?;
 
     let comms = setup_p2p_rpc(config, comms, message_senders, peer_provider);
@@ -153,6 +154,11 @@ pub async fn spawn_services(
     // Save final node identity after comms has initialized. This is required because the public_address can be
     // changed by comms during initialization when using tor.
     save_identities(config, &comms)?;
+
+    // Auto-registration
+    if config.validator_node.auto_register {
+        auto_registration::spawn(config.clone(), node_identity.clone(), epoch_manager.clone(), shutdown);
+    }
 
     Ok(Services {
         comms,
