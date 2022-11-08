5ba1e970ad3b7c3a6626c8a731dfb2e9da7f4bdf
Martin Stefcek <gcifko@gmail.com>
2022-11-08 13:53:47 +0100

    feat: add sorting to recent TX in webgui

--- applications/tari_validator_node_web_ui/src/RecentTransactions.tsx
+++ applications/tari_validator_node_web_ui/src/RecentTransactions.tsx
@@ -33,13 +33,50 @@ interface IRecentTransaction {
   total_votes: number;
 }
 
+interface ITableRecentTransaction {
+  id: string;
+  height: number;
+  payload_height: number;
+  payload_id: string;
+  shard: string;
+  total_leader_proposals: number;
+  total_votes: number;
+}
+
+type ColumnKey = keyof ITableRecentTransaction;
+
 function RecentTransactions() {
-  const [recentTransacations, setRecentTransacations] = useState<IRecentTransaction[]>([]);
+  const [recentTransacations, setRecentTransacations] = useState<ITableRecentTransaction[]>([]);
+  const [lastSort, setLastSort] = useState({ column: "", order: -1 });
   useEffect(() => {
     getRecentTransactions().then((response) => {
-      setRecentTransacations(response.transactions);
+      setRecentTransacations(
+        response.transactions.map(
+          ({ height, payload_height, payload_id, shard, total_leader_proposals, total_votes }: IRecentTransaction) => ({
+            id: payload_height + toHexString(payload_id),
+            height,
+            payload_height,
+            payload_id: toHexString(payload_id),
+            shard: toHexString(shard),
+            total_leader_proposals,
+            total_votes,
+          })
+        )
+      );
     });
   }, []);
+  const sort = (column: ColumnKey) => {
+    let order = 1;
+    if (lastSort.column === column) {
+      order = -lastSort.order;
+    }
+    setRecentTransacations(
+      [...recentTransacations].sort((r0, r1) =>
+        r0[column] > r1[column] ? order : r0[column] < r1[column] ? -order : 0
+      )
+    );
+    setLastSort({ column, order });
+  };
   if (recentTransacations === undefined) {
     return (
       <div className="section">
@@ -53,22 +90,52 @@ function RecentTransactions() {
       <table className="recent-transactions-table">
         <thead>
           <tr>
-            <th className="column">Height</th>
-            <th className="column">Payload height</th>
-            <th className="column">Payload id</th>
-            <th className="column">Shard</th>
-            <th className="column">Total leader proposal</th>
-            <th className="column">Total votes</th>
+            <th className="column" onClick={() => sort("height")}>
+              Height
+              <span className="sort-indicator">
+                {lastSort.column === "height" ? (lastSort.order === 1 ? "▲" : "▼") : ""}
+              </span>
+            </th>
+            <th className="column" onClick={() => sort("payload_height")}>
+              Payload height
+              <span className="sort-indicator">
+                {lastSort.column === "payload_height" ? (lastSort.order === 1 ? "▲" : "▼") : ""}
+              </span>
+            </th>
+            <th className="column" onClick={() => sort("payload_id")}>
+              Payload id
+              <span className="sort-indicator">
+                {lastSort.column === "payload_id" ? (lastSort.order === 1 ? "▲" : "▼") : ""}
+              </span>
+            </th>
+            <th className="column" onClick={() => sort("shard")}>
+              Shard
+              <span className="sort-indicator">
+                {lastSort.column === "shard" ? (lastSort.order === 1 ? "▲" : "▼") : ""}
+              </span>
+            </th>
+            <th className="column" onClick={() => sort("total_leader_proposals")}>
+              Total leader proposal
+              <span className="sort-indicator">
+                {lastSort.column === "total_leader_proposals" ? (lastSort.order === 1 ? "▲" : "▼") : ""}
+              </span>
+            </th>
+            <th className="column" onClick={() => sort("total_votes")}>
+              Total votes
+              <span className="sort-indicator">
+                {lastSort.column === "total_votes" ? (lastSort.order === 1 ? "▲" : "▼") : ""}
+              </span>
+            </th>
           </tr>
         </thead>
         <tbody>
           {recentTransacations.map(
-            ({ height, payload_height, payload_id, shard, total_leader_proposals, total_votes }) => (
-              <tr key={payload_height + toHexString(payload_id)}>
+            ({ id, height, payload_height, payload_id, shard, total_leader_proposals, total_votes }) => (
+              <tr key={id}>
                 <td>{height}</td>
                 <td>{payload_height}</td>
-                <td className="key">{toHexString(payload_id)}</td>
-                <td className="key">{toHexString(shard)}</td>
+                <td className="key">{payload_id}</td>
+                <td className="key">{shard}</td>
                 <td>{total_leader_proposals}</td>
                 <td>{total_votes}</td>
               </tr>
