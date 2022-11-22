6805904ae4ed2609c14970b132faa6f9a4a7683c
Stan Bondi <sdbondi@users.noreply.github.com>
2022-11-22 11:29:12 +0400

    fix licence

--- applications/tari_validator_node_cli/src/command/manifest.template.rs
+++ applications/tari_validator_node_cli/src/command/manifest.template.rs
@@ -1,3 +1,6 @@
+//   Copyright 2022 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Claus
+
 // TODO: Improve this template
 use template_bc617551fc331bb5ebc12943c1456dbf8fbd0ae4e950b2ffc28a6b89ce040382 as Account;
 
