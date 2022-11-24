5fd8129f6a91105a9451fae19ce64be263641439
stringhandler <mikethetike@tari.com>
2022-11-24 22:41:56 +0200

    feat: determine leader from payload

--- dan_layer/core/src/services/leader_strategy.rs
+++ dan_layer/core/src/services/leader_strategy.rs
@@ -20,6 +20,8 @@
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+use digest::Digest;
+use tari_crypto::hash::blake2::Blake256;
 use tari_dan_common_types::{PayloadId, ShardId};
 
 use crate::{models::Committee, services::infrastructure_services::NodeAddressable};
@@ -65,5 +67,20 @@ impl<TAddr: NodeAddressable> LeaderStrategy<TAddr> for AlwaysFirstLeader {
         _round: u32,
     ) -> u32 {
         0
+    }
+}
+
+pub struct PayloadSpecificLeaderStrategy {}
+impl<TAddr: NodeAddressable> LeaderStrategy<TAddr> for PayloadSpecificLeaderStrategy {
+    fn calculate_leader(&self, committee: &Committee<TAddr>, payload: PayloadId, shard: ShardId, round: u32) -> u32 {
+        // Perhaps a less heavy hasher in future?
+        let hash: Vec<u8> = Blake256::new()
+            .chain(payload.as_bytes())
+            .chain(shard.as_bytes())
+            .finalize()
+            .to_vec();
+        let hash = u32::from_le_bytes([hash[0], hash[1], hash[2], hash[3]]);
+        let first = (hash % committee.members.len() as u32) as u32;
+        (first + round) % committee.members.len() as u32
     }
 }
