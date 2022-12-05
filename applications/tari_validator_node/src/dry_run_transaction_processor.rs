4ae3ce13bffa3d589a8f43fc86a18e7bbed4e298
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2022-12-05 12:17:16 +0000

    process transactions with local pledges

--- applications/tari_validator_node/src/dry_run_transaction_processor.rs
+++ applications/tari_validator_node/src/dry_run_transaction_processor.rs
@@ -1,3 +1,11 @@
+use std::collections::HashMap;
+
+use tari_dan_common_types::ShardId;
+use tari_dan_core::{
+    models::{ObjectPledge, Payload, TariDanPayload},
+    services::{epoch_manager::EpochManager, PayloadProcessor},
+    storage::shard_store::{ShardStoreFactory, ShardStoreTransaction},
+};
 use tari_dan_engine::transaction::Transaction;
 use tari_dan_storage_sqlite::sqlite_shard_store_factory::SqliteShardStoreFactory;
 use tari_engine_types::commit_result::FinalizeResult;
@@ -10,43 +18,81 @@ use crate::{
 #[derive(Clone)]
 pub struct DryRunTransactionProcessor {
     /// The epoch manager
-    _epoch_manager: EpochManagerHandle,
+    epoch_manager: EpochManagerHandle,
     /// The payload processor. This determines whether a payload proposal results in an accepted or rejected vote.
-    _payload_processor: TariDanPayloadProcessor<TemplateManager>,
+    payload_processor: TariDanPayloadProcessor<TemplateManager>,
     /// Store used to persist consensus state.
-    _shard_store: SqliteShardStoreFactory,
+    shard_store: SqliteShardStoreFactory,
 }
 
 impl DryRunTransactionProcessor {
     pub fn new(
-        _epoch_manager: EpochManagerHandle,
-        _payload_processor: TariDanPayloadProcessor<TemplateManager>,
-        _shard_store: SqliteShardStoreFactory,
+        epoch_manager: EpochManagerHandle,
+        payload_processor: TariDanPayloadProcessor<TemplateManager>,
+        shard_store: SqliteShardStoreFactory,
     ) -> Self {
         Self {
-            _epoch_manager,
-            _payload_processor,
-            _shard_store,
+            epoch_manager,
+            payload_processor,
+            shard_store,
         }
     }
 
     // TODO: create a error enum
-    pub async fn process_transaction(&self, _transaction: Transaction) -> Result<FinalizeResult, String> {
-        dbg!(&self._epoch_manager);
-        dbg!(&self._payload_processor);
-        dbg!(&self._shard_store);
-        Err("TODO".to_owned())
+    pub async fn process_transaction(&self, transaction: Transaction) -> Result<FinalizeResult, String> {
+        // get the list of involved shards for the transaction
+        let payload = TariDanPayload::new(transaction.clone());
+        let involved_shards = payload.involved_shards();
+
+        // get the local shard state
+        let _epoch = self.epoch_manager.current_epoch().await.map_err(|e| e.to_string());
+
+        // get the pledges for all local shards
+        let shard_pledges = self.get_local_pledges(involved_shards).await?;
+
+        // TODO: get non local shard pledges
+
+        let result = self
+            .payload_processor
+            .process_payload(payload, shard_pledges)
+            .map_err(|e| e.to_string())?;
+        Ok(result)
+    }
+
+    async fn get_local_pledges(
+        &self,
+        involved_shards: Vec<ShardId>,
+    ) -> Result<HashMap<ShardId, Option<ObjectPledge>>, String> {
+        dbg!(&involved_shards);
+        let tx = self.shard_store.create_tx().unwrap();
+        let inventory = tx.get_state_inventory().unwrap();
+        dbg!(&inventory);
+
+        let local_shard_ids: Vec<ShardId> = involved_shards.into_iter().filter(|s| inventory.contains(s)).collect();
+        dbg!(&local_shard_ids);
+        let mut local_pledges = HashMap::new();
+        for shard_id in local_shard_ids {
+            // TODO: create a DB method to get the substates of a list of shards
+            let substate_data = tx
+                .get_substate_states(shard_id, shard_id, &[])
+                .unwrap()
+                .first()
+                .unwrap()
+                .clone();
+            dbg!(&substate_data);
+            let local_pledge = ObjectPledge {
+                shard_id,
+                current_state: substate_data.substate().clone(),
+                pledged_to_payload: substate_data.payload_id(),
+                pledged_until: substate_data.height(),
+            };
+            local_pledges.insert(shard_id, Some(local_pledge));
+        }
+        dbg!(&local_pledges);
+        Ok(local_pledges)
     }
 }
 
-// BUILD THE PAYLOAD
-// let payload = TariDanPayload::new(transaction.clone());
-//
-// GET THE LIST OF INVOLVED SHARDS
-// fn involved_shards(&self) -> Vec<ShardId>;
-// payload.invoved_shards
-//
-//
 // let mut tx = self.shard_store.create_tx()?;
 // let high_qc = tx.get_high_qc_for(shard).optional()?.unwrap_or_else(|| {
 // TODO: sign genesis
