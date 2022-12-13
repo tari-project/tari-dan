a67cb2a9f6f091744225a5c53308fea0f0c456d4
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2022-12-13 08:53:29 +0000

    ignore clippy on builtin templates

--- dan_layer/template_builtin/src/lib.rs
+++ dan_layer/template_builtin/src/lib.rs
@@ -20,6 +20,9 @@
 //  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //  USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+// The Github CI tries to fmt and check inside the templates folder
+#![allow(clippy::all)]
+
 pub fn account_wasm() -> Vec<u8> {
     include_bytes!("../templates/account/account.wasm").to_vec()
 }
