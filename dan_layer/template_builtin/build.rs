87dbe2f913dca5a349b09af9fec2bcb697b91f83
Stan Bondi <sdbondi@users.noreply.github.com>
2023-02-03 16:39:28 +0400

    feat(account-builtin): adds access rules to account component

--- dan_layer/template_builtin/build.rs
+++ dan_layer/template_builtin/build.rs
@@ -33,6 +33,8 @@ use std::{
 const TEMPLATE_BUILTINS: &[&str] = &["templates/account"];
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Rebuild templates if lib changed
+    println!("cargo:rerun-if-changed=../template_lib");
     for template in TEMPLATE_BUILTINS {
         // we only want to rebuild if a template was added/modified
         println!("cargo:rerun-if-changed={}/src", template);
