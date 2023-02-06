80c461324ba49ef2f4c9bf2d014f0277a7f7d6de
Miguel Naveira <47919901+mrnaveira@users.noreply.github.com>
2023-02-06 05:53:59 +0000

    feat(integration_test): new cucumber NFT scenario (#347)
    Description
    ---
    * Created a new cucumber scenario that mints, mutates and burns NFTs.
    * Needed to add an ad-hoc way to manifests to easily declare the new NFT substates of a transaction, so I specify them through special comments (`$mint` and `$mint_specific`). In the future, when the substate calculation is delegated to the VNs, this workaround will be not necessary.
    
    Motivation and Context
    ---
    Integration tests must cover the new NFT functionality
    
    How Has This Been Tested?
    ---
    The new cucumber scenario passes

--- applications/tari_validator_node/tests/utils/validator_node_cli.rs
+++ applications/tari_validator_node/tests/utils/validator_node_cli.rs
@@ -1,13 +1,23 @@
 //  Copyright 2022 The Tari Project
 //  SPDX-License-Identifier: BSD-3-Clause
 
-use std::{path::PathBuf, str::FromStr};
+use std::{collections::HashMap, path::PathBuf, str::FromStr};
 
 use tari_engine_types::substate::{SubstateAddress, SubstateDiff};
 use tari_template_builtin::ACCOUNT_TEMPLATE_ADDRESS;
-use tari_transaction_manifest::parse_manifest;
+use tari_template_lib::prelude::NonFungibleId;
+use tari_transaction_manifest::{parse_manifest, ManifestValue};
 use tari_validator_node_cli::{
-    command::transaction::{handle_submit, submit_transaction, CliArg, CliInstruction, CommonSubmitArgs, SubmitArgs},
+    command::transaction::{
+        handle_submit,
+        submit_transaction,
+        CliArg,
+        CliInstruction,
+        CommonSubmitArgs,
+        NewNonFungibleMintOutput,
+        SpecificNonFungibleMintOutput,
+        SubmitArgs,
+    },
     from_hex::FromHex,
     key_manager::KeyManager,
     versioned_substate_address::VersionedSubstateAddress,
@@ -232,7 +242,7 @@ pub async fn submit_manifest(
 ) {
     let input_groups = inputs.split(',').map(|s| s.trim()).collect::<Vec<_>>();
     // generate globals for components addresses
-    let globals = world
+    let globals: HashMap<String, ManifestValue> = world
         .outputs
         .iter()
         .filter(|(name, _)| input_groups.contains(&name.as_str()))
@@ -240,6 +250,38 @@ pub async fn submit_manifest(
             outputs
                 .iter()
                 .map(move |(child_name, addr)| (format!("{}/{}", name, child_name), addr.address.clone().into()))
+        })
+        .collect();
+
+    // parse the minting outputs (if any) specified in the manifest as comments
+    let new_non_fungible_outputs: Vec<NewNonFungibleMintOutput> = manifest_content
+        .lines()
+        .filter(|l| l.starts_with("// $mint "))
+        .map(|l| l.split_whitespace().skip(2).collect::<Vec<&str>>())
+        .map(|l| {
+            let manifest_value = globals.get(l[0]).unwrap();
+            let resource_address = manifest_value.address().unwrap().as_resource_address().unwrap();
+            let count = l[1].parse().unwrap();
+            NewNonFungibleMintOutput {
+                resource_address,
+                count,
+            }
+        })
+        .collect();
+
+    // parse the minting specific outputs (if any) specified in the manifest as comments
+    let non_fungible_mint_outputs: Vec<SpecificNonFungibleMintOutput> = manifest_content
+        .lines()
+        .filter(|l| l.starts_with("// $mint_specific "))
+        .map(|l| l.split_whitespace().skip(2).collect::<Vec<&str>>())
+        .map(|l| {
+            let manifest_value = globals.get(l[0]).unwrap();
+            let resource_address = manifest_value.address().unwrap().as_resource_address().unwrap();
+            let non_fungible_id = NonFungibleId::try_from_canonical_string(l[1]).unwrap();
+            SpecificNonFungibleMintOutput {
+                resource_address,
+                non_fungible_id,
+            }
         })
         .collect();
 
@@ -277,8 +319,8 @@ pub async fn submit_manifest(
         dump_outputs_into: None,
         account_template_address: None,
         dry_run: false,
-        non_fungible_mint_outputs: vec![],
-        new_non_fungible_outputs: vec![],
+        non_fungible_mint_outputs,
+        new_non_fungible_outputs,
     };
     let resp = submit_transaction(instructions, args, data_dir, &mut client)
         .await
