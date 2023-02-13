d8f435548e94952b8372d110b872d104ec61cb85
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2023-02-13 15:20:55 +0000

    get the lastest version of a substate

--- applications/tari_indexer/src/json_rpc/handlers.rs
+++ applications/tari_indexer/src/json_rpc/handlers.rs
@@ -20,7 +20,7 @@
 //   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 //   USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use std::str::FromStr;
+use std::{convert::TryInto, str::FromStr};
 
 use axum_jrpc::{
     error::{JsonRpcError, JsonRpcErrorReason},
@@ -33,7 +33,8 @@ use log::info;
 use serde::Serialize;
 use serde_json::{self as json, json};
 use tari_comms::CommsNode;
-use tari_dan_common_types::ShardId;
+use tari_crypto::ristretto::RistrettoPublicKey;
+use tari_dan_common_types::{Epoch, ShardId, SubstateState};
 use tari_dan_core::services::{epoch_manager::EpochManager, BaseNodeClient, ValidatorNodeClientFactory};
 use tari_dan_storage_sqlite::sqlite_shard_store_factory::SqliteShardStore;
 use tari_engine_types::substate::{Substate, SubstateAddress};
@@ -120,43 +121,19 @@ impl JsonRpcHandlers {
         let answer_id = value.get_answer_id();
         let substate_address_str: String = value.parse_params()?;
         let substate_address = SubstateAddress::from_str(&substate_address_str).unwrap();
-        let version = 0;
-        let shard_id = ShardId::from_address(&substate_address, version);
-
         let epoch = self.epoch_manager.current_epoch().await.unwrap();
-        let committee = self.epoch_manager.get_committee(epoch, shard_id).await.unwrap();
 
-        for vn_public_key in committee.members {
-            // build a client with the VN
-            let mut sync_vn_client = self.validator_node_client_factory.create_client(&vn_public_key);
-            let mut sync_vn_rpc_client = sync_vn_client.create_connection().await.unwrap();
+        let mut latest_substate = None;
+        let mut version = 0;
 
-            // request the shard substate to the VN
-            let shard_id_proto: crate::p2p::proto::common::ShardId = shard_id.into();
-            let request = crate::p2p::proto::rpc::VnStateSyncRequest {
-                start_shard_id: Some(shard_id_proto.clone()),
-                end_shard_id: Some(shard_id_proto),
-                inventory: vec![],
-            };
+        // we keep asking from version 0 upwards until a version does not exist
+        while let Some(substate) = self.get_substate_from_commitee(&substate_address, version, epoch).await {
+            latest_substate = Some(substate);
+            version += 1;
+        }
 
-            // get the VN's response
-            let mut vn_state_stream = match sync_vn_rpc_client.vn_state_sync(request).await {
-                Ok(stream) => stream,
-                Err(e) => {
-                    info!(target: LOG_TARGET, "Unable to connect to peer: {} ", e);
-                    // we do not stop when an indiviual VN does not respond, we try all VNs
-                    continue;
-                },
-            };
-
-            // return the substate from the response
-            if let Some(resp) = vn_state_stream.next().await {
-                let resp = resp.unwrap();
-                match extract_substate_from_vn_sync_response(resp).unwrap() {
-                    Some(substate) => return Ok(JsonRpcResponse::success(answer_id, substate)),
-                    None => break,
-                }
-            }
+        if let Some(substate) = latest_substate {
+            return Ok(JsonRpcResponse::success(answer_id, substate));
         }
 
         Err(JsonRpcResponse::error(
@@ -168,15 +145,79 @@ impl JsonRpcHandlers {
             ),
         ))
     }
-}
 
-fn extract_substate_from_vn_sync_response(msg: VnStateSyncResponse) -> Result<Option<Substate>, anyhow::Error> {
-    if msg.destroyed_payload_id.is_empty() && !msg.created_payload_id.is_empty() {
-        let substate = Substate::from_bytes(&msg.substate)?;
-        return Ok(Some(substate));
+    async fn get_substate_from_commitee(
+        &self,
+        substate_address: &SubstateAddress,
+        version: u32,
+        epoch: Epoch,
+    ) -> Option<Substate> {
+        let shard_id = ShardId::from_address(substate_address, version);
+        let committee = self.epoch_manager.get_committee(epoch, shard_id).await.unwrap();
+
+        for vn_public_key in &committee.members {
+            let res = self.get_substate_state_from_vn(vn_public_key, shard_id, version).await;
+
+            if let Ok(SubstateState::Up { data: substate, .. }) = res {
+                return Some(substate);
+            }
+        }
+
+        None
     }
 
-    Ok(None)
+    async fn get_substate_state_from_vn(
+        &self,
+        vn_public_key: &RistrettoPublicKey,
+        shard_id: ShardId,
+        _version: u32,
+    ) -> Result<SubstateState, anyhow::Error> {
+        // build a client with the VN
+        let mut sync_vn_client = self.validator_node_client_factory.create_client(vn_public_key);
+        let mut sync_vn_rpc_client = sync_vn_client.create_connection().await?;
+
+        // request the shard substate to the VN
+        let shard_id_proto: crate::p2p::proto::common::ShardId = shard_id.into();
+        let request = crate::p2p::proto::rpc::VnStateSyncRequest {
+            start_shard_id: Some(shard_id_proto.clone()),
+            end_shard_id: Some(shard_id_proto),
+            inventory: vec![],
+        };
+
+        // get the VN's response
+        let mut vn_state_stream = match sync_vn_rpc_client.vn_state_sync(request).await {
+            Ok(stream) => stream,
+            Err(e) => {
+                info!(target: LOG_TARGET, "Unable to connect to peer: {} ", e);
+                return Err(e.into());
+            },
+        };
+
+        // return the substate from the response (there is going to be only 0 or 1 result)
+        if let Some(msg) = vn_state_stream.next().await {
+            let msg = msg?;
+            let state = extract_state_from_vn_response(msg)?;
+            return Ok(state);
+        }
+
+        Ok(SubstateState::DoesNotExist)
+    }
+}
+
+fn extract_state_from_vn_response(msg: VnStateSyncResponse) -> Result<SubstateState, anyhow::Error> {
+    let state = if let Some(deleted_by) = Some(msg.destroyed_payload_id).filter(|p| !p.is_empty()) {
+        SubstateState::Down {
+            deleted_by: deleted_by.try_into()?,
+        }
+    } else {
+        let substate = Substate::from_bytes(&msg.substate)?;
+        SubstateState::Up {
+            created_by: msg.created_payload_id.try_into()?,
+            data: substate,
+        }
+    };
+
+    Ok(state)
 }
 
 #[derive(Serialize, Debug)]
