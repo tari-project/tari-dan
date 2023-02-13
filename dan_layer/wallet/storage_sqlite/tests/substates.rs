a0d10e4ae8ed361ccfeb11e64c235f916d39dac5
Stan Bondi <sdbondi@users.noreply.github.com>
2023-02-13 13:03:12 +0400

    update get_and_insert_substates test

--- dan_layer/wallet/storage_sqlite/tests/substates.rs
+++ dan_layer/wallet/storage_sqlite/tests/substates.rs
@@ -1,9 +1,16 @@
 //   Copyright 2023 The Tari Project
 //   SPDX-License-Identifier: BSD-3-Clause
 
+use std::str::FromStr;
+
+use tari_common_types::types::FixedHash;
 use tari_dan_common_types::optional::Optional;
-use tari_dan_wallet_sdk::storage::{WalletStore, WalletStoreReader};
+use tari_dan_wallet_sdk::{
+    models::VersionedSubstateAddress,
+    storage::{WalletStore, WalletStoreReader, WalletStoreWriter},
+};
 use tari_dan_wallet_storage_sqlite::SqliteWalletStore;
+use tari_engine_types::{substate::SubstateAddress, TemplateAddress};
 
 #[test]
 fn get_and_insert_substates() {
@@ -14,14 +21,42 @@ fn get_and_insert_substates() {
     let db = SqliteWalletStore::try_open(":memory:").unwrap();
     db.run_migrations().unwrap();
     let tx = db.create_write_tx().unwrap();
-    let address = tx.substates_get(&example_addr).optional().unwrap();
-    assert!(address.is_none());
-    // let hash = *transaction.hash();
-    // tx.transactions_insert(&transaction).unwrap();
-    // tx.commit().unwrap();
+    let substate = tx.substates_get(&example_addr).optional().unwrap();
+    assert!(substate.is_none());
+    let hash = FixedHash::zero();
+    let address =
+        SubstateAddress::from_str("component_1f019e4d434cbf2b99c0af89ee212f422af86de7280a169d2e392dfb66ab34d4")
+            .unwrap();
+    tx.substates_insert_parent(
+        hash,
+        VersionedSubstateAddress {
+            address: address.clone(),
+            version: 0,
+        },
+        "".to_string(),
+        TemplateAddress::default(),
+    )
+    .unwrap();
 
-    // let tx = db.create_read_tx().unwrap();
-    // let returned = tx.transaction_get(hash.into_array().into()).unwrap();
-    // assert_eq!(transaction, returned.transaction);
-    // assert_eq!(returned.status, TransactionStatus::default());
+    let child_address =
+        SubstateAddress::from_str("component_d9e4a7ce7dbaa73ce10aabf309dd702054756a813f454ef13564f298887bb69d")
+            .unwrap();
+    tx.substates_insert_child(hash, address.clone(), VersionedSubstateAddress {
+        address: child_address.clone(),
+        version: 0,
+    })
+    .unwrap();
+
+    tx.commit().unwrap();
+
+    let tx = db.create_read_tx().unwrap();
+    let returned = tx.substates_get(&address).unwrap();
+    assert!(returned.parent_address.is_none());
+    assert_eq!(returned.address.address, address);
+    assert_eq!(returned.address.version, 0);
+
+    let returned = tx.substates_get(&child_address).unwrap();
+    assert_eq!(returned.parent_address, Some(address));
+    assert_eq!(returned.address.address, child_address);
+    assert_eq!(returned.address.version, 0);
 }
