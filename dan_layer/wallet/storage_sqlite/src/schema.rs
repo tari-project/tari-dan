2a6976e0eadb665dfc5227472347a24410b7ac31
Stan Bondi <sdbondi@users.noreply.github.com>
2023-02-14 13:25:07 +0400

    cargofmt

--- dan_layer/wallet/storage_sqlite/src/schema.rs
+++ dan_layer/wallet/storage_sqlite/src/schema.rs
@@ -64,10 +64,4 @@ diesel::table! {
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(
-    accounts,
-    config,
-    key_manager_states,
-    substates,
-    transactions,
-);
+diesel::allow_tables_to_appear_in_same_query!(accounts, config, key_manager_states, substates, transactions,);
