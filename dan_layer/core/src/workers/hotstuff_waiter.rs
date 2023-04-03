d47613b39b308069bbf18fe58b09177153cf08c6
jorgeantonio21 <matroid@outlook.com>
2023-04-03 18:00:25 +0100

    add fees accrued on waiter

--- dan_layer/core/src/workers/hotstuff_waiter.rs
+++ dan_layer/core/src/workers/hotstuff_waiter.rs
@@ -850,6 +850,16 @@ where
         // Check if we have resolved all pledges, if so, we are ready to commit resultant substate changes
         if resolved_pledges.len() == involved_shards.len() {
             let payload_result = tx.get_payload_result(&node.payload_id())?;
+            // let total_fee_accrued = 0;
+            let total_fee_accrued = payload_result
+                .exec_result
+                .clone()
+                .fee_receipt
+                .unwrap()
+                .total_fee_payment
+                .as_u64_checked()
+                .ok_or(HotStuffError::MathOverflow)?;
+
             match &payload_result.exec_result.finalize.result {
                 TransactionResult::Accept(diff) => {
                     if resolved_pledges
@@ -864,7 +874,8 @@ where
                         return Ok(());
                     }
 
-                    let local_change_set = extract_changes_for_shards(&local_shards, node.payload_id(), diff)?;
+                    let local_change_set =
+                        extract_changes_for_shards(&local_shards, node.payload_id(), diff, total_fee_accrued)?;
                     for pledge in resolved_pledges {
                         // Only persist local shards
                         if let Some(changes) = local_change_set.get(&pledge.shard_id) {
@@ -1917,7 +1928,13 @@ fn extract_changes_for_shards(
     shard_ids: &[ShardId],
     payload_id: PayloadId,
     diff: &SubstateDiff,
+    total_fee_accrued: u64,
 ) -> Result<HashMap<ShardId, Vec<SubstateState>>, HotStuffError> {
+    // we split the total accrued fee over all substate differences
+    let total_diffs = diff.len() as u64;
+    let accrued_fee_per_diff = total_fee_accrued
+        .checked_div(total_diffs)
+        .ok_or(HotStuffError::MathOverflow)?;
     let mut changes = HashMap::<_, Vec<_>>::new();
     // down first, then up
     for (address, version) in diff.down_iter() {
@@ -1925,7 +1942,7 @@ fn extract_changes_for_shards(
         if shard_ids.contains(&shard_id) {
             changes.entry(shard_id).or_default().push(SubstateState::Down {
                 deleted_by: payload_id,
-                fees_accrued: 0,
+                fees_accrued: accrued_fee_per_diff,
             });
         }
     }
@@ -1936,7 +1953,7 @@ fn extract_changes_for_shards(
                 address: address.clone(),
                 created_by: payload_id,
                 data: substate.clone(),
-                fees_accrued: 0,
+                fees_accrued: accrued_fee_per_diff,
             });
         }
     }
