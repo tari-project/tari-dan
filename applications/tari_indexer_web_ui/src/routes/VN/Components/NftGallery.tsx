f91aed09037138cd87cd4c3232b0468f659cf2e8
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2023-04-04 21:18:41 +0100

    create dummy nft gallery

--- applications/tari_indexer_web_ui/src/routes/VN/Components/NftGallery.tsx
+++ applications/tari_indexer_web_ui/src/routes/VN/Components/NftGallery.tsx
@@ -45,7 +45,7 @@ import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Collapse from '@mui/material/Collapse';
 import TablePagination from '@mui/material/TablePagination';
 import Typography from '@mui/material/Typography';
-import { Button, TextField } from '@mui/material';
+import { Button, ImageList, ImageListItem, ImageListItemBar, TextField } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { ConfirmDialog } from '../../../Components/AlertDialog';
 
@@ -63,7 +63,7 @@ function NftGallery() {
           let nft_data = nft.substate.substate.NonFungible.data['@@TAGGED@@'][1];
           let {image_url, name} = nft_data;
           console.log(image_url);
-          console.log(nft_data);
+          console.log(name);
         });
       });
     }
@@ -73,8 +73,87 @@ function NftGallery() {
     updateCollection();
   }, []);
 
+  const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
+      author: '@bkristastucchio',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
+      author: '@rollelflex_graphy726',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
+      author: '@helloimnik',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
+      author: '@nolanissac',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Hats',
+      author: '@hjrc33',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+      title: 'Honey',
+      author: '@arwinneil',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+      title: 'Basketball',
+      author: '@tjdragotta',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+      title: 'Fern',
+      author: '@katie_wasserman',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+      title: 'Mushrooms',
+      author: '@silverdalex',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+      title: 'Tomato basil',
+      author: '@shelleypauls',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+      title: 'Sea star',
+      author: '@peterlaster',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+      title: 'Bike',
+      author: '@southside_customs',
+    },
+  ];
+
   return (
-    <Typography variant="h4">Monitored NFT collections ... loading</Typography>
+    <ImageList cols={4} gap={8}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+        <img
+          src={`${item.img}?w=248&fit=crop&auto=format`}
+          srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 4x`}
+          alt={item.title}
+          loading="lazy"
+        />
+        <ImageListItemBar
+          title={item.title}
+          subtitle={<span>by: {item.author}</span>}
+          position="below"
+        />
+      </ImageListItem>
+    ))}
+  </ImageList>
   );
 }
 
