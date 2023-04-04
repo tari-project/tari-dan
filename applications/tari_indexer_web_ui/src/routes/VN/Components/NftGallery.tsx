2465b942d0bfa076fcd90f34030b8c1431ed0e77
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2023-04-04 21:39:19 +0100

    adding proper nft images

--- applications/tari_indexer_web_ui/src/routes/VN/Components/NftGallery.tsx
+++ applications/tari_indexer_web_ui/src/routes/VN/Components/NftGallery.tsx
@@ -75,64 +75,34 @@ function NftGallery() {
 
   const itemData = [
     {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'Breakfast',
-      author: '@bkristastucchio',
+      img: 'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149622024.jpg',
+      title: 'Astronaut (Image by Freepik.com)',
+      index: 0,
     },
     {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Burger',
-      author: '@rollelflex_graphy726',
+      img: 'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149629576.jpg',
+      title: 'Baby  (Image by Freepik.com)',
+      index: 1,
     },
     {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Camera',
-      author: '@helloimnik',
+      img: 'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149622021.jpg',
+      title: 'Cool (Image by Freepik.com)',
+      index: 2,
     },
     {
-      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      title: 'Coffee',
-      author: '@nolanissac',
+      img: 'https://img.freepik.com/premium-vector/hand-drawn-monkey-ape-vr-box-virtual-nft-style_361671-246.jpg',
+      title: 'Metaverse (Image by Freepik.com)',
+      index: 3,
     },
     {
-      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-      title: 'Hats',
-      author: '@hjrc33',
+      img: 'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149629594.jpg',
+      title: 'Suit (Image by Freepik.com)',
+      index: 4,
     },
     {
-      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-      title: 'Honey',
-      author: '@arwinneil',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-      title: 'Basketball',
-      author: '@tjdragotta',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-      title: 'Fern',
-      author: '@katie_wasserman',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-      title: 'Mushrooms',
-      author: '@silverdalex',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-      title: 'Tomato basil',
-      author: '@shelleypauls',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-      title: 'Sea star',
-      author: '@peterlaster',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-      title: 'Bike',
-      author: '@southside_customs',
+      img: 'https://img.freepik.com/free-vector/hand-drawn-nft-style-ape-illustration_23-2149629582.jpg',
+      title: 'Cook  (Image by Freepik.com)',
+      index: 5,
     },
   ];
 
@@ -141,14 +111,14 @@ function NftGallery() {
       {itemData.map((item) => (
         <ImageListItem key={item.img}>
         <img
-          src={`${item.img}?w=248&fit=crop&auto=format`}
-          srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 4x`}
+          src={`${item.img}?size=248&fit=fill&auto=format`}
+          srcSet={`${item.img}?size=248&fit=fill&auto=format&dpr=2 4x`}
           alt={item.title}
           loading="lazy"
         />
         <ImageListItemBar
           title={item.title}
-          subtitle={<span>by: {item.author}</span>}
+          subtitle={<span># {item.index}</span>}
           position="below"
         />
       </ImageListItem>
