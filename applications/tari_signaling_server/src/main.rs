36ffb0d90f20a99074bde04f90bb4c85ed6131ba
Martin Stefcek <35243812+Cifko@users.noreply.github.com>
2023-04-06 15:29:14 +0200

    feat: add signaling server (#456)
    Description
    ---
    The signaling server is a jrpc server. Which generate a jwt token, that
    token then maps to offer/answer/ice_candidates.
    
    Motivation and Context
    ---
    
    How Has This Been Tested?
    ---
    Manually.
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify

---
+++ applications/tari_signaling_server/src/main.rs
@@ -0,0 +1,35 @@
+//   Copyright 2022 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Clause
+
+mod cli;
+mod data;
+mod jrpc_server;
+
+use std::error::Error;
+
+use cli::Cli;
+use data::Data;
+use tari_common::initialize_logging;
+use tari_shutdown::Shutdown;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::init();
+
+    let shutdown = Shutdown::new();
+    let shutdown_signal = shutdown.to_signal();
+
+    if let Err(e) = initialize_logging(
+        cli.base_dir().join("config/logs.yml").as_path(),
+        &cli.base_dir(),
+        include_str!("../log4rs_sample.yml"),
+    ) {
+        eprintln!("{}", e);
+    }
+
+    let data = Data::new();
+
+    let address = cli.listen_address();
+    jrpc_server::listen(address, data, shutdown_signal).await?;
+    Ok(())
+}
