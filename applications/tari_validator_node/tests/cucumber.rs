9659019e634886ff9ea9c10cd0e43a3d15baed6b
jorgeantonio21 <matroid@outlook.com>
2023-05-05 10:32:37 +0100

    cargo fmt

--- applications/tari_validator_node/tests/cucumber.rs
+++ applications/tari_validator_node/tests/cucumber.rs
@@ -26,7 +26,12 @@ use std::{collections::HashMap, convert::TryFrom, future, io, str::FromStr, time
 
 use cucumber::{
     gherkin::{Scenario, Step},
-    given, then, when, writer, World, WriterExt,
+    given,
+    then,
+    when,
+    writer,
+    World,
+    WriterExt,
 };
 use indexmap::IndexMap;
 use tari_common::initialize_logging;
@@ -43,7 +48,11 @@ use tari_dan_engine::abi::Type;
 use tari_template_lib::Hash;
 use tari_validator_node_cli::versioned_substate_address::VersionedSubstateAddress;
 use tari_validator_node_client::types::{
-    AddPeerRequest, GetIdentityResponse, GetRecentTransactionsRequest, GetTemplateRequest, GetTransactionResultRequest,
+    AddPeerRequest,
+    GetIdentityResponse,
+    GetRecentTransactionsRequest,
+    GetTemplateRequest,
+    GetTransactionResultRequest,
     TemplateRegistrationResponse,
 };
 use utils::{
