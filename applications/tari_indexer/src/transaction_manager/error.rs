e0812fdf8788f567fdad8e60c7e71b3156ac0204
Stan Bondi <sdbondi@users.noreply.github.com>
2023-05-05 17:30:59 +0400

    fix(vn): only return payload result if finalized

--- applications/tari_indexer/src/transaction_manager/error.rs
+++ applications/tari_indexer/src/transaction_manager/error.rs
@@ -1,10 +1,20 @@
 //   Copyright 2023 The Tari Project
 //   SPDX-License-Identifier: BSD-3-Clause
 
+use tari_dan_common_types::optional::IsNotFoundError;
+
 #[derive(Debug, Clone, thiserror::Error)]
 pub enum TransactionManagerError {
     #[error("Committee provider error: {0}")]
     CommitteeProviderError(String),
     #[error("Rpc call failed for all ({committee_size}) validators")]
     AllValidatorsFailed { committee_size: usize },
+    #[error("{entity} not found: {key}")]
+    NotFound { entity: &'static str, key: String },
+}
+
+impl IsNotFoundError for TransactionManagerError {
+    fn is_not_found_error(&self) -> bool {
+        matches!(self, Self::NotFound { .. })
+    }
 }
