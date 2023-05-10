55e44a820818aa27435db998018d5a266a446178
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2023-05-10 16:25:33 +0100

    adding liquidity

--- dan_layer/engine/tests/tariswap.rs
+++ dan_layer/engine/tests/tariswap.rs
@@ -16,6 +16,7 @@ struct TariSwapTest {
     template_test: TemplateTest,
     a_resource: ResourceAddress,
     b_resource: ResourceAddress,
+    lp_resource: ResourceAddress,
     tariswap: ComponentAddress,
     account_address: ComponentAddress,
     account_proof: NonFungibleAddress,
@@ -28,7 +29,7 @@ fn setup() -> TariSwapTest {
     let (a_faucet, a_resource) = create_faucet_component(&mut template_test, "A".to_string());
     let (b_faucet, b_resource) = create_faucet_component(&mut template_test, "B".to_string());
 
-    let tariswap = create_tariswap_component(&mut template_test, a_faucet, b_faucet);
+    let (tariswap, lp_resource) = create_tariswap_component(&mut template_test, a_resource, b_resource);
 
     let (account_address, account_proof, _) = template_test.create_owned_account();
     fund_account(&mut template_test, account_address, a_faucet);
@@ -38,6 +39,7 @@ fn setup() -> TariSwapTest {
         template_test,
         a_resource,
         b_resource,
+        lp_resource,
         tariswap,
         account_address,
         account_proof,
@@ -59,41 +61,29 @@ fn create_faucet_component(template_test: &mut TemplateTest, symbol: String) -> 
 
 fn create_tariswap_component(
     template_test: &mut TemplateTest,
-    a_faucet: ComponentAddress,
-    b_faucet: ComponentAddress,
-) -> ComponentAddress {
+    a_resource: ResourceAddress,
+    b_resource: ResourceAddress,
+) -> (ComponentAddress, ResourceAddress) {
     let module_name = "TariSwapPool";
     let tariswap_template = template_test.get_template_address(module_name);
 
     let res = template_test
         .execute_and_commit(
             vec![
-                Instruction::CallMethod {
-                    component_address: a_faucet,
-                    method: "take_free_coins".to_string(),
-                    args: args![],
-                },
-                Instruction::PutLastInstructionOutputOnWorkspace {
-                    key: b"a_bucket".to_vec(),
-                },
-                Instruction::CallMethod {
-                    component_address: b_faucet,
-                    method: "take_free_coins".to_string(),
-                    args: args![],
-                },
-                Instruction::PutLastInstructionOutputOnWorkspace {
-                    key: b"b_bucket".to_vec(),
-                },
                 Instruction::CallFunction {
                     template_address: tariswap_template,
                     function: "new".to_string(),
-                    args: args![Variable("a_bucket"), Variable("b_bucket")],
+                    args: args![a_resource, b_resource],
+                },
+                Instruction::PutLastInstructionOutputOnWorkspace {
+                    key: b"lp_bucket".to_vec(),
                 },
             ],
             vec![],
         )
         .unwrap();
 
+    // extract the component address
     let (substate_addr, _) = res
         .expect_success()
         .up_iter()
@@ -101,8 +91,17 @@ fn create_tariswap_component(
             address.is_component() && substate.substate_value().component().unwrap().module_name == module_name
         })
         .unwrap();
+    let component_address = substate_addr.as_component_address().unwrap();
 
-    substate_addr.as_component_address().unwrap()
+    // extract the LP token resource address
+    let (substate_addr, _) = res
+        .expect_success()
+        .up_iter()
+        .find(|(address, _)| address.is_resource())
+        .unwrap();
+    let lp_resource = substate_addr.as_resource_address().unwrap();
+
+    (component_address, lp_resource)
 }
 
 fn fund_account(
@@ -132,12 +131,7 @@ fn fund_account(
         .unwrap();
 }
 
-fn do_swap(
-    test: &mut TariSwapTest,
-    input_resource: &ResourceAddress,
-    output_resource: &ResourceAddress,
-    amount: Amount,
-) {
+fn swap(test: &mut TariSwapTest, input_resource: &ResourceAddress, output_resource: &ResourceAddress, amount: Amount) {
     test.template_test
         .execute_and_commit(
             vec![
@@ -169,6 +163,46 @@ fn do_swap(
         .unwrap();
 }
 
+fn add_liduidity(test: &mut TariSwapTest, a_amount: Amount, b_amount: Amount) {
+    test.template_test
+        .execute_and_commit(
+            vec![
+                Instruction::CallMethod {
+                    component_address: test.account_address,
+                    method: "withdraw".to_string(),
+                    args: args![test.a_resource, a_amount],
+                },
+                Instruction::PutLastInstructionOutputOnWorkspace {
+                    key: b"a_bucket".to_vec(),
+                },
+                Instruction::CallMethod {
+                    component_address: test.account_address,
+                    method: "withdraw".to_string(),
+                    args: args![test.b_resource, b_amount],
+                },
+                Instruction::PutLastInstructionOutputOnWorkspace {
+                    key: b"b_bucket".to_vec(),
+                },
+                Instruction::CallMethod {
+                    component_address: test.tariswap,
+                    method: "add_liquidity".to_string(),
+                    args: args![Variable("a_bucket"), Variable("b_bucket")],
+                },
+                Instruction::PutLastInstructionOutputOnWorkspace {
+                    key: b"lp_bucket".to_vec(),
+                },
+                Instruction::CallMethod {
+                    component_address: test.account_address,
+                    method: "deposit".to_string(),
+                    args: args![Variable("lp_bucket"),],
+                },
+            ],
+            // proof needed to withdraw
+            vec![test.account_proof.clone()],
+        )
+        .unwrap();
+}
+
 fn get_pool_balance(test: &mut TariSwapTest, resource_address: ResourceAddress) -> Amount {
     test.template_test
         .call_method(test.tariswap, "get_pool_balance", args![resource_address], vec![])
@@ -180,11 +214,12 @@ fn get_account_balance(test: &mut TariSwapTest, resource_address: ResourceAddres
 }
 
 #[test]
-fn swap() {
+fn add_liquidity_and_swap() {
     // init the test
     let mut test = setup();
     let a_resource = test.a_resource;
     let b_resource = test.b_resource;
+    let lp_resource = test.lp_resource;
 
     // check initial balances
     let mut account_a_balance = get_account_balance(&mut test, a_resource);
@@ -194,14 +229,34 @@ fn swap() {
 
     let mut pool_a_balance = get_pool_balance(&mut test, a_resource);
     let mut pool_b_balance = get_pool_balance(&mut test, b_resource);
-    assert_eq!(pool_a_balance, INITIAL_BALANCE);
-    assert_eq!(pool_b_balance, INITIAL_BALANCE);
+    assert_eq!(pool_a_balance, 0);
+    assert_eq!(pool_b_balance, 0);
+
+    // ------- ADD LIQUIDITY -------
+    let liquidity_amount = Amount::new(500);
+    add_liduidity(&mut test, liquidity_amount, liquidity_amount);
+
+    // check account balances
+    account_a_balance = get_account_balance(&mut test, a_resource);
+    account_b_balance = get_account_balance(&mut test, a_resource);
+    assert_eq!(account_a_balance, INITIAL_BALANCE - liquidity_amount);
+    assert_eq!(account_b_balance, INITIAL_BALANCE - liquidity_amount);
+
+    // the account should have now LP tokens
+    let account_lp_balance = get_account_balance(&mut test, lp_resource);
+    assert_eq!(account_lp_balance, liquidity_amount * 2); // we provided both "a" and "b" tokens
+
+    // check pool balances
+    pool_a_balance = get_pool_balance(&mut test, a_resource);
+    pool_b_balance = get_pool_balance(&mut test, b_resource);
+    assert_eq!(pool_a_balance, liquidity_amount);
+    assert_eq!(pool_b_balance, liquidity_amount);
 
     // ------- FIRST SWAP -------
-    // do a swap, giving 100 "A" tokens for "B" tokens
-    let input_amount = Amount::new(100);
-    let expected_output_amount = Amount::new(91); // following the constant product formula: b = k / a
-    do_swap(&mut test, &a_resource, &b_resource, input_amount);
+    // do a swap, giving "A" tokens for "B" tokens
+    let input_amount = Amount::new(50);
+    let expected_output_amount = Amount::new(46); // following the constant product formula: b = k / a
+    swap(&mut test, &a_resource, &b_resource, input_amount);
 
     // check that the new pool balances are expected
     let new_pool_a_balance = get_pool_balance(&mut test, a_resource);
@@ -222,9 +277,9 @@ fn swap() {
     // ------- SECOND SWAP -------
     // do another swap
     // this time we are providing "B" tokens which are more scarce now, so we receive a more of "A" tokens in return
-    let input_amount = Amount::new(100);
-    let expected_output_amount = Amount::new(110); // following the constant product formula: b = k / a
-    do_swap(&mut test, &b_resource, &a_resource, input_amount);
+    let input_amount = Amount::new(50);
+    let expected_output_amount = Amount::new(55); // following the constant product formula: b = k / a
+    swap(&mut test, &b_resource, &a_resource, input_amount);
     assert_eq!(
         get_pool_balance(&mut test, a_resource),
         pool_a_balance - expected_output_amount
--- dan_layer/engine/tests/templates/tariswap/src/lib.rs
+++ dan_layer/engine/tests/templates/tariswap/src/lib.rs
@@ -32,39 +32,46 @@ mod tariswap {
     // TODO: pay market fee to LP holders (market fee as a constructor param)
     // TODO: lp resource minting/burning security
     pub struct TariSwapPool {
-        pool_vaults: HashMap<ResourceAddress, Vault>,
+        pools: HashMap<ResourceAddress, Vault>,
+        lp_resource: ResourceAddress,
     }
 
     impl TariSwapPool {
 
         // Initialises a new pool component for for the pool A - B
-        pub fn new(a: Bucket, b: Bucket) -> Self {
+        pub fn new(a_addr: ResourceAddress, b_addr: ResourceAddress) -> Self {
             // check the the resource pair is correct
-            assert!(a.resource_address() != b.resource_address(), "The tokens to swap are the same");
+            assert!(a_addr != b_addr, "The resources of the pair must be different");
             assert!(
-                a.resource_type() == ResourceType::Fungible,
+                ResourceManager::get(a_addr).resource_type() == ResourceType::Fungible,
                 "Resource 'a' is not fungible"
             );
             assert!(
-                b.resource_type() == ResourceType::Fungible,
+                ResourceManager::get(a_addr).resource_type() == ResourceType::Fungible,
                 "Resource 'b' is not fungible"
             );
 
             // create the vaults to store the funds
-            let mut pool_vaults = HashMap::new();
-            pool_vaults.insert(a.resource_address(), Vault::from_bucket(a));
-            pool_vaults.insert(b.resource_address(), Vault::from_bucket(b));
+            let mut pools = HashMap::new();
+            pools.insert(a_addr, Vault::new_empty(a_addr));
+            pools.insert(b_addr, Vault::new_empty(b_addr));
+
+            // create the lp resource
+            let lp_resource = ResourceBuilder::fungible("LP").build();
 
             Self {
-                pool_vaults,
+                pools,
+                lp_resource,
             }
         }
 
         // swap A tokens for B tokens or viceversa
         pub fn swap(&mut self, input_bucket: Bucket, output_resource: ResourceAddress) -> Bucket {
-            // get the data needed to calculate the pool rebalancing
+            // check that the parameters are correct
             let input_resource = input_bucket.resource_address();
-            assert!(input_resource != output_resource, "The resource addresses are the same");
+            self.check_pool_resources(input_resource, output_resource);
+
+            // get the data needed to calculate the pool rebalancing
             let input_balance = self.get_pool_balance(input_resource);
             let output_balance = self.get_pool_balance(output_resource);
 
@@ -83,20 +90,44 @@ mod tariswap {
             let output_bucket_amount = output_balance - new_output_balance;
 
             // perform the swap
-            self.pool_vaults.get_mut(&input_resource).unwrap().deposit(input_bucket);
-            self.pool_vaults.get_mut(&output_resource).unwrap().withdraw(output_bucket_amount)
+            self.pools.get_mut(&input_resource).unwrap().deposit(input_bucket);
+            self.pools.get_mut(&output_resource).unwrap().withdraw(output_bucket_amount)
         }
 
-        // TODO: add liquidity
+        pub fn add_liquidity(&mut self, a_bucket: Bucket, b_bucket: Bucket) -> Bucket {
+            // check that the buckets are correct
+            let a_resource = a_bucket.resource_address();
+            let b_resource = b_bucket.resource_address();
+            self.check_pool_resources(a_resource, b_resource);
 
-        // TODO: remove liquidity
+            // extract the bucket amounts for later
+            let a_amount = a_bucket.amount();
+            let b_amount = b_bucket.amount();
 
+            // add the liquidity to the pool
+            self.pools.get_mut(&a_resource).unwrap().deposit(a_bucket);
+            self.pools.get_mut(&b_resource).unwrap().deposit(b_bucket);
+
+            // get the bucket/pool ratios
+            let a_ratio = self.get_pool_ratio(a_resource, a_amount);
+            let b_ratio = self.get_pool_ratio(b_resource, b_amount);
+
+            // the amount of new lp tokens are proportional to the bucket-pool ratios
+            let new_lp_amount = a_ratio * a_amount + b_ratio * b_amount;
+
+            // mint and return the new lp tokens
+            ResourceManager::get(self.lp_resource).mint_fungible(new_lp_amount)
+        }
+
+        // TODO: remove liquidity.
+        // Right now we cannot implement it as we need to process tuple variables in the workspace
+        // pub fn remove_liquidity(&mut self, lp_bucket: Bucket) -> (Bucket, Bucket)
 
         // public utility methods
         pub fn get_pool_balances(&self) -> HashMap<ResourceAddress, Amount> {
             let mut balances = HashMap::new();
 
-            for (resource, vault) in &self.pool_vaults {
+            for (resource, vault) in &self.pools {
                 balances.insert(resource.clone(), vault.balance());
             }
 
@@ -104,14 +135,33 @@ mod tariswap {
         }
         
         pub fn get_pool_balance(&self, resource_address: ResourceAddress) -> Amount {
-            let vault = self.pool_vaults.get(&resource_address)
+            let vault = self.pools.get(&resource_address)
                 .unwrap_or_else(|| panic!("Resource {} is not in the pool", resource_address));
             vault.balance()
         }
+
+        pub fn get_pool_ratio(&self, resource: ResourceAddress, amount: Amount) -> Amount {
+            let balance = self.get_pool_balance(resource);
+
+            if balance == 0 {
+                Amount::new(1)
+            } else {
+                amount / balance
+            }
+        }
          
+        pub fn lp_resource(&self) -> ResourceAddress {
+            self.lp_resource
+        }
 
-        // TODO: get LP token address and supply
+        pub fn lp_total_supply(&self) -> Amount {
+            ResourceManager::get(self.lp_resource).total_supply()
+        }
 
-        // TODO: get token price
+        fn check_pool_resources(&self, a_resource: ResourceAddress, b_resource: ResourceAddress) {
+            assert!(a_resource != b_resource, "The resource addresses are the same");
+            assert!(self.pools.contains_key(&a_resource), "The resource {} is not in the pool", a_resource);
+            assert!(self.pools.contains_key(&b_resource), "The resource {} is not in the pool", b_resource);
+        }
     }
 }
