81a3fec793897259372e91173483b456ac317b18
Cifko <gcifko@gmail.com>
2023-05-16 00:30:10 +0200

    chore: bump the tari version

--- applications/tari_indexer/src/comms/initializer.rs
+++ applications/tari_indexer/src/comms/initializer.rs
@@ -292,6 +292,6 @@ async fn initialize_hidden_service(
         builder = builder.with_tor_identity(identity);
     }
 
-    let hidden_svc_ctl = builder.build().await?;
+    let hidden_svc_ctl = builder.build()?;
     Ok(hidden_svc_ctl)
 }
