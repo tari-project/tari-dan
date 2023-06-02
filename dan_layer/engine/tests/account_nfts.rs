e9efc6c2cdc0967e10430c6aeb5c13aafcc200e2
jorgeantonio21 <matroid@outlook.com>
2023-06-02 13:34:23 +0100

    feat: add account nft template (#541)
    Description
    ---
    Creates a built-in template for accounts minting NFTs.
    
    Motivation and Context
    ---
    
    How Has This Been Tested?
    ---
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify

---
+++ dan_layer/engine/tests/account_nfts.rs
@@ -0,0 +1,71 @@
+//   Copyright 2023 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Clause
+
+use tari_engine_types::instruction::Instruction;
+use tari_template_lib::{args, models::ComponentAddress, prelude::Metadata};
+use tari_template_test_tooling::TemplateTest;
+
+#[test]
+fn basic_nft_mint() {
+    let mut account_nft_template_test = TemplateTest::new(vec!["../template_builtin/templates/account_nfts/"]);
+
+    let account_nft_template = account_nft_template_test.get_template_address("AccountNonFungible");
+
+    let (owner_component_address, owner_token, _) = account_nft_template_test.create_owned_account();
+    let token_symbol: &str = "ACCNFT";
+
+    let result = account_nft_template_test
+        .execute_and_commit(
+            vec![Instruction::CallFunction {
+                template_address: account_nft_template,
+                function: "create".to_string(),
+                args: args![owner_token, token_symbol],
+            }],
+            vec![],
+        )
+        .unwrap();
+
+    assert!(result.finalize.result.is_accept());
+    let nft_component_address: ComponentAddress = result.finalize.execution_results[0].decode().unwrap();
+
+    let result = account_nft_template_test
+        .execute_and_commit(
+            vec![Instruction::CallMethod {
+                component_address: nft_component_address,
+                method: "get_resource_address".to_string(),
+                args: args![],
+            }],
+            vec![],
+        )
+        .unwrap();
+
+    assert!(result.finalize.is_accept());
+
+    let mut metadata = Metadata::new();
+
+    metadata.insert("name".to_string(), "my_custom_nft".to_string());
+    metadata.insert("brightness".to_string(), "100".to_string());
+
+    let result = account_nft_template_test
+        .execute_and_commit(
+            vec![
+                Instruction::CallMethod {
+                    component_address: nft_component_address,
+                    method: "mint".to_string(),
+                    args: args![metadata],
+                },
+                Instruction::PutLastInstructionOutputOnWorkspace {
+                    key: b"my_nft".to_vec(),
+                },
+                Instruction::CallMethod {
+                    component_address: owner_component_address,
+                    method: "deposit".to_string(),
+                    args: args![Variable("my_nft")],
+                },
+            ],
+            vec![owner_token],
+        )
+        .unwrap();
+
+    assert!(result.finalize.result.is_accept());
+}
