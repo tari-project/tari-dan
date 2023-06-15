bf7ebadd5bd0641dca77f9100dafe55d8835be26
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2023-06-15 10:24:46 +0100

    clean up test code

--- dan_layer/engine/tests/composability.rs
+++ dan_layer/engine/tests/composability.rs
@@ -13,20 +13,57 @@ use tari_template_lib::{
 };
 use tari_template_test_tooling::TemplateTest;
 use tari_transaction::Transaction;
-fn setup() -> TemplateTest {
-    TemplateTest::new(vec![
+
+struct ComposabilityTest {
+    template_test: TemplateTest,
+    composability_template: TemplateAddress,
+    state_template: TemplateAddress,
+}
+
+struct ComposabilityComponentInfo {
+    composability_component: ComponentAddress,
+    state_component: ComponentAddress,
+}
+
+fn setup() -> ComposabilityTest {
+    let template_test = TemplateTest::new(vec![
         "tests/templates/composability",
         "tests/templates/state",
         "tests/templates/faucet",
-    ])
+    ]);
+
+    let composability_template = template_test.get_template_address("Composability");
+    let state_template = template_test.get_template_address("State");
+
+    ComposabilityTest {
+        template_test,
+        composability_template,
+        state_template,
+    }
 }
 
-fn get_state_template_address(test: &TemplateTest) -> TemplateAddress {
-    test.get_template_address("State")
-}
+fn initialize_composability(test: &mut ComposabilityTest) -> ComposabilityComponentInfo {
+    // the composability template "new" function should create a new "state" component as well
+    let res = test
+        .template_test
+        .execute_and_commit(
+            vec![Instruction::CallFunction {
+                template_address: test.composability_template,
+                function: "new".to_string(),
+                args: args![test.state_template],
+            }],
+            vec![],
+        )
+        .unwrap();
 
-fn get_composability_template_address(test: &TemplateTest) -> TemplateAddress {
-    test.get_template_address("Composability")
+    // extract the newly created component addresses
+    let composability_component = extract_component_address_from_result(&res, "Composability");
+    let state_component = extract_component_address_from_result(&res, "State");
+
+    ComposabilityComponentInfo {
+        composability_component,
+        state_component,
+    }
 }
 
 fn extract_component_address_from_result(result: &ExecuteResult, template_name: &str) -> ComponentAddress {
@@ -91,194 +128,132 @@ fn create_resource_and_fund_account(test: &mut TemplateTest, account: ComponentA
 #[test]
 fn it_allows_function_to_function_calls() {
     let mut test = setup();
-    let state_template = get_state_template_address(&test);
-    let composability_template = get_composability_template_address(&test);
-
-    // the composability template "new" function should create a new "state" component as well
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-
-    // extract the newly created component addresses
-    let composability_component = extract_component_address_from_result(&res, "Composability");
-    let state_component = extract_component_address_from_result(&res, "State");
+    let components = initialize_composability(&mut test);
 
     // the composability component exists in the network and is correctly initialized
-    let inner_component_address: ComponentAddress =
-        test.call_method(composability_component, "get_state_component_address", args![], vec![]);
-    assert_eq!(inner_component_address, state_component);
+    let inner_component_address: ComponentAddress = test.template_test.call_method(
+        components.composability_component,
+        "get_state_component_address",
+        args![],
+        vec![],
+    );
+    assert_eq!(inner_component_address, components.state_component);
 
     // the state component exists in the network and is correctly initialized
-    let value: u32 = test.call_method(state_component, "get", args![], vec![]);
+    let value: u32 = test
+        .template_test
+        .call_method(components.state_component, "get", args![], vec![]);
     assert_eq!(value, 0);
 }
 
 #[test]
 fn it_allows_function_to_method_calls() {
     let mut test = setup();
-    let state_template = get_state_template_address(&test);
-    let composability_template = get_composability_template_address(&test);
-
-    // the composability template "new" function should create a new "state" component as well
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-
-    // extract the newly created component addresses
-    let composability_a_component = extract_component_address_from_result(&res, "Composability");
-    let state_component = extract_component_address_from_result(&res, "State");
+    let components = initialize_composability(&mut test);
+    let composability_component_0 = components.composability_component;
 
     // create a new composability component, this time using a constructor that gets information from a method call
     let res = test
+        .template_test
         .execute_and_commit(
             vec![Instruction::CallFunction {
-                template_address: composability_template,
+                template_address: test.composability_template,
                 function: "new_from_component".to_string(),
-                args: args![composability_a_component],
+                args: args![composability_component_0],
             }],
             vec![],
         )
         .unwrap();
-    let composability_b_component = extract_component_address_from_result(&res, "Composability");
+    let composability_component_1 = extract_component_address_from_result(&res, "Composability");
 
     // the composability component exists in the network and is correctly initialized
-    let inner_component_address: ComponentAddress = test.call_method(
-        composability_b_component,
+    let inner_component_address: ComponentAddress = test.template_test.call_method(
+        composability_component_1,
         "get_state_component_address",
         args![],
         vec![],
     );
-    assert_eq!(inner_component_address, state_component);
+    assert_eq!(inner_component_address, components.state_component);
 }
 
 #[test]
 fn it_allows_method_to_method_calls() {
     let mut test = setup();
-    let state_template = get_state_template_address(&test);
-    let composability_template = get_composability_template_address(&test);
-
-    // the composability template "new" function should create a new "state" component as well
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-
-    // extract the newly created component addresses
-    let composability_component = extract_component_address_from_result(&res, "Composability");
-    let state_component = extract_component_address_from_result(&res, "State");
+    let components = initialize_composability(&mut test);
 
     // the state component has an initial value of 0
-    let value: u32 = test.call_method(state_component, "get", args![], vec![]);
+    let value: u32 = test
+        .template_test
+        .call_method(components.state_component, "get", args![], vec![]);
     assert_eq!(value, 0);
 
     // perform the call to the composability component that will increase the counter
-    test.call_method::<()>(
-        composability_component,
+    test.template_test.call_method::<()>(
+        components.composability_component,
         "increase_inner_state_component",
         args![],
         vec![],
     );
 
     // the state component has been increased
-    let value: u32 = test.call_method(state_component, "get", args![], vec![]);
+    let value: u32 = test
+        .template_test
+        .call_method(components.state_component, "get", args![], vec![]);
     assert_eq!(value, 1);
 }
 
 #[test]
 fn it_allows_method_to_function_calls() {
     let mut test = setup();
-    let state_template = get_state_template_address(&test);
-    let composability_template = get_composability_template_address(&test);
-
-    // the composability template "new" function should create a new "state" component as well
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-
-    // extract the newly created component addresses
-    let composability_component = extract_component_address_from_result(&res, "Composability");
-    let initial_state_component = extract_component_address_from_result(&res, "State");
+    let components = initialize_composability(&mut test);
+    let initial_state_component = components.state_component;
 
     // perform the call to the composability component that will increase the counter
-    test.call_method::<()>(
-        composability_component,
+    test.template_test.call_method::<()>(
+        components.composability_component,
         "increase_inner_state_component",
         args![],
         vec![],
     );
-    let value: u32 = test.call_method(initial_state_component, "get", args![], vec![]);
+    let value: u32 = test
+        .template_test
+        .call_method(initial_state_component, "get", args![], vec![]);
     assert_eq!(value, 1);
 
     // perform the call to replace the inner state component for a new one
-    test.call_method::<()>(
-        composability_component,
+    test.template_test.call_method::<()>(
+        components.composability_component,
         "replace_state_component",
-        args![state_template],
+        args![test.state_template],
         vec![],
     );
 
     // a new state component should have been initialized
-    let new_state_component: ComponentAddress =
-        test.call_method(composability_component, "get_state_component_address", args![], vec![]);
+    let new_state_component: ComponentAddress = test.template_test.call_method(
+        components.composability_component,
+        "get_state_component_address",
+        args![],
+        vec![],
+    );
     assert_ne!(new_state_component, initial_state_component);
-    let value: u32 = test.call_method(new_state_component, "get", args![], vec![]);
+    let value: u32 = test
+        .template_test
+        .call_method(new_state_component, "get", args![], vec![]);
     assert_eq!(value, 0);
 }
 
 #[test]
 fn it_fails_on_invalid_calls() {
     let mut test = setup();
-    let (_, _, private_key) = test.create_owned_account();
-    let state_template = get_state_template_address(&test);
-    let composability_template = get_composability_template_address(&test);
-
-    // the composability template "new" function should create a new "state" component as well
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-
-    // extract the newly created component addresses
-    let composability_component = extract_component_address_from_result(&res, "Composability");
+    let components = initialize_composability(&mut test);
+    let (_, _, private_key) = test.template_test.create_owned_account();
 
     // the "invalid_state_call" method tries to call a non-existent method in the inner state component
     let result = test
+        .template_test
         .try_execute_and_commit(
             Transaction::builder()
-                .call_method(composability_component, "invalid_state_call", args![])
+                .call_method(components.composability_component, "invalid_state_call", args![])
                 .sign(&private_key)
                 .build(),
             vec![],
@@ -294,34 +269,19 @@ fn it_fails_on_invalid_calls() {
 #[test]
 fn it_does_not_propagate_permissions() {
     let mut test = setup();
-    let (account, owner_proof, private_key) = test.create_owned_account();
-    let state_template = get_state_template_address(&test);
-    let composability_template = get_composability_template_address(&test);
-
-    // the composability template "new" function should create a new "state" component as well
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-
-    // extract the newly created component addresses
-    let composability_component = extract_component_address_from_result(&res, "Composability");
+    let components = initialize_composability(&mut test);
+    let (account, owner_proof, private_key) = test.template_test.create_owned_account();
 
     // create_resource_and_fund_account
-    let fungible_resource = create_resource_and_fund_account(&mut test, account);
+    let fungible_resource = create_resource_and_fund_account(&mut test.template_test, account);
 
     // try to to an account withdraw inside the composability template, it should fail as the owner proof should not be
     // propagated
     let result = test
+        .template_test
         .try_execute_and_commit(
             Transaction::builder()
-                .call_method(composability_component, "malicious_withdraw", args![
+                .call_method(components.composability_component, "malicious_withdraw", args![
                     account,
                     fungible_resource,
                     100
@@ -342,35 +302,15 @@ fn it_does_not_propagate_permissions() {
 #[test]
 fn it_allows_multiple_recursion_levels() {
     let mut test = setup();
-    let state_template = get_state_template_address(&test);
-    let composability_template = get_composability_template_address(&test);
 
     // composability_0
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-    let composability_0 = extract_component_address_from_result(&res, "Composability");
+    let mut components = initialize_composability(&mut test);
+    let composability_0 = components.composability_component;
 
     // composability_1 has composability_0 nested
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-    let composability_1 = extract_component_address_from_result(&res, "Composability");
-    test.call_method::<()>(
+    components = initialize_composability(&mut test);
+    let composability_1 = components.composability_component;
+    test.template_test.call_method::<()>(
         composability_1,
         "set_nested_composability",
         args![composability_0],
@@ -379,54 +319,40 @@ fn it_allows_multiple_recursion_levels() {
 
     // we have now: composability_1 -> composability_0 -> state
     // we want to access the innermost level from the outermost level
-    let value: u32 = test.call_method(composability_1, "get_nested_value", args![], vec![]);
+    let value: u32 = test
+        .template_test
+        .call_method(composability_1, "get_nested_value", args![], vec![]);
     assert_eq!(value, 0);
 }
 
 #[test]
 fn it_fails_when_surpassing_recursion_limit() {
     let mut test = setup();
-    let (_, _, private_key) = test.create_owned_account();
-    let state_template = get_state_template_address(&test);
-    let composability_template = get_composability_template_address(&test);
-
+    let (_, _, private_key) = test.template_test.create_owned_account();
     let max_recursion_depth = ConsensusConstants::devnet().max_call_recursion_depth;
 
     // innermost composability component
-    let res = test
-        .execute_and_commit(
-            vec![Instruction::CallFunction {
-                template_address: composability_template,
-                function: "new".to_string(),
-                args: args![state_template],
-            }],
-            vec![],
-        )
-        .unwrap();
-    let mut last_component = extract_component_address_from_result(&res, "Composability");
+    let mut components = initialize_composability(&mut test);
+    let mut last_composability_component = components.composability_component;
 
     for _ in 0..max_recursion_depth {
-        let res = test
-            .execute_and_commit(
-                vec![Instruction::CallFunction {
-                    template_address: composability_template,
-                    function: "new".to_string(),
-                    args: args![state_template],
-                }],
-                vec![],
-            )
-            .unwrap();
-        let component = extract_component_address_from_result(&res, "Composability");
-        test.call_method::<()>(component, "set_nested_composability", args![last_component], vec![]);
-        last_component = component;
+        components = initialize_composability(&mut test);
+        test.template_test.call_method::<()>(
+            components.composability_component,
+            "set_nested_composability",
+            args![last_composability_component],
+            vec![],
+        );
+        last_composability_component = components.composability_component;
     }
 
     // we have now nested more components than the recursion depth limit allows
     // se when we do a call that goes from the outermost to the innermost, it must fail
     let result = test
+        .template_test
         .try_execute_and_commit(
             Transaction::builder()
-                .call_method(last_component, "get_nested_value", args![])
+                .call_method(last_composability_component, "get_nested_value", args![])
                 .sign(&private_key)
                 .build(),
             vec![],
