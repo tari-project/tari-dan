e241786218725e2f2a8ea5d123293fe0ed4ca967
Cifko <gcifko@gmail.com>
2023-06-26 21:58:30 +0200

    fix clippy

--- applications/tari_dan_wallet_daemon/src/handlers/nfts.rs
+++ applications/tari_dan_wallet_daemon/src/handlers/nfts.rs
@@ -13,9 +13,7 @@ use tari_dan_wallet_sdk::{
     models::Account,
 };
 use tari_engine_types::{
-    component::new_component_address_from_parts,
-    instruction::Instruction,
-    substate::SubstateAddress,
+    component::new_component_address_from_parts, instruction::Instruction, substate::SubstateAddress,
 };
 use tari_template_builtin::ACCOUNT_NFT_TEMPLATE_ADDRESS;
 use tari_template_lib::{
@@ -26,13 +24,8 @@ use tari_template_lib::{
 use tari_transaction::{SubstateRequirement, Transaction};
 use tari_utilities::ByteArray;
 use tari_wallet_daemon_client::types::{
-    AccountNftInfo,
-    GetAccountNftRequest,
-    GetAccountNftResponse,
-    ListAccountNftRequest,
-    ListAccountNftResponse,
-    MintAccountNftRequest,
-    MintAccountNftResponse,
+    AccountNftInfo, GetAccountNftRequest, GetAccountNftResponse, ListAccountNftRequest, ListAccountNftResponse,
+    MintAccountNftRequest, MintAccountNftResponse,
 };
 use tokio::sync::broadcast;
 
@@ -51,7 +44,7 @@ pub async fn handle_get_nft(
     req: GetAccountNftRequest,
 ) -> Result<GetAccountNftResponse, anyhow::Error> {
     let sdk = context.wallet_sdk();
-    sdk.jwt_api().check_auth(token.clone(), &[JrpcPermission::Admin])?;
+    sdk.jwt_api().check_auth(token, &[JrpcPermission::Admin])?;
 
     let non_fungible_api = sdk.non_fungible_api();
 
@@ -77,7 +70,7 @@ pub async fn handle_list_nfts(
 ) -> Result<ListAccountNftResponse, anyhow::Error> {
     let ListAccountNftRequest { limit, offset, .. } = req;
     let sdk = context.wallet_sdk();
-    sdk.jwt_api().check_auth(token.clone(), &[JrpcPermission::Admin])?;
+    sdk.jwt_api().check_auth(token, &[JrpcPermission::Admin])?;
 
     let non_fungible_api = sdk.non_fungible_api();
 
@@ -287,10 +280,11 @@ async fn create_account_nft(
     let transaction = Transaction::builder()
         .fee_transaction_pay_from_component(account.address.as_component_address().unwrap(), fee)
         .with_inputs(inputs)
-        .call_function(*ACCOUNT_NFT_TEMPLATE_ADDRESS, "create", args![
-            owner_token,
-            token_symbol
-        ])
+        .call_function(
+            *ACCOUNT_NFT_TEMPLATE_ADDRESS,
+            "create",
+            args![owner_token, token_symbol],
+        )
         .sign(owner_sk)
         .build();
 
