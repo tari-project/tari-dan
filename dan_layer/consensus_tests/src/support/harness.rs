558f7e8556eb3206531366e1c85578aeb611a4bf
Stan Bondi <sdbondi@users.noreply.github.com>
2023-07-07 16:11:37 +0200

    feat(consensus): implement simpler batching co-ordination (#577)
    Description
    ---
    - feat: use Prepare, LocalPrepared, and Accept commands in block
    - refactor: use single transaction pool with New, LocalPrepared,
    AllPrepared, SomePrepared stages
    - feat: BlockCommitted event
    - feat: replaced pledges with substate multi-read/single-write lock 
    - test: implement multi-shard consensus tests
    - test: implement virtual network that connects all consensus instances 
    
    Motivation and Context
    ---
    Implement ideas from the [chainspace
    paper](https://arxiv.org/abs/1708.03778) - thanks @stringhandler for
    conceptualising this.
    
    How Has This Been Tested?
    ---
    Implemented multi-shard functional tests in the `consensus_tests` crate.
    
    What process can a PR reviewer use to test or verify this change?
    ---
    Only by running the tests as consensus has not been integrated with the
    validator node yet
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify
    
    ---------
    
    Co-authored-by: stringhandler <mikethetike@tari.com>

--- dan_layer/consensus_tests/src/support/harness.rs
+++ dan_layer/consensus_tests/src/support/harness.rs
@@ -1,33 +1,33 @@
 //   Copyright 2023 The Tari Project
 //   SPDX-License-Identifier: BSD-3-Clause
 
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 
-use futures::{future::join_all, FutureExt};
-use tari_consensus::{
-    messages::{ProposalMessage, VoteMessage},
-    traits::{EpochManager, LeaderStrategy},
-};
-use tari_dan_common_types::NodeHeight;
+use futures::{stream::FuturesUnordered, StreamExt};
+use tari_consensus::{hotstuff::HotstuffEvent, traits::EpochManager};
+use tari_dan_common_types::committee::Committee;
 use tari_dan_storage::{
-    consensus_models::{BlockId, Decision, TransactionPool},
+    consensus_models::{Block, BlockId, Decision, TransactionPoolStage},
     StateStore,
     StateStoreReadTransaction,
 };
+use tokio::task;
 
 use crate::support::{
     address::TestAddress,
     epoch_manager::TestEpochManager,
+    network::{spawn_network, TestNetwork},
     transaction::build_transaction,
     validator::Validator,
     SelectedIndexLeaderStrategy,
+    ValidatorChannels,
 };
 
 pub struct Test {
-    validators: BTreeMap<TestAddress, Validator>,
-    leader_strategy: SelectedIndexLeaderStrategy,
+    validators: HashMap<TestAddress, Validator>,
+    network: TestNetwork,
+    _leader_strategy: SelectedIndexLeaderStrategy,
     epoch_manager: TestEpochManager,
-    messages_sent: HashMap<DanMessageType, usize>,
 }
 
 impl Test {
@@ -35,68 +35,48 @@ impl Test {
         TestBuilder::new()
     }
 
-    pub fn total_messages_sent(&self) -> usize {
-        self.messages_sent.values().sum()
+    pub async fn send_transaction_to_all(&self, decision: Decision, fee: u64, num_shards: usize) {
+        let tx = build_transaction(decision, fee, num_shards);
+        self.network.send_transaction(tx).await;
     }
 
-    pub async fn do_hotstuff_round(&mut self, round: u64) {
-        let (leader, proposal) = self.assert_proposal_broadcast().await;
-        self.send_proposal_to_all(leader, &proposal).await;
-
-        self.forward_all_votes_to_leader().await;
-        self.wait_for_all_to_have_leaf_height(NodeHeight(round)).await;
+    pub async fn on_hotstuff_event(&mut self) -> HotstuffEvent {
+        self.validators
+            .values_mut()
+            .map(|v| v.events.recv())
+            .collect::<FuturesUnordered<_>>()
+            .next()
+            .await
+            .unwrap()
+            .unwrap()
     }
 
-    pub async fn send_all_transaction(&self, decision: Decision, fee: u64) {
-        let tx = build_transaction(decision, fee);
-        for validator in self.validators.values() {
-            validator.send_transaction(&tx).await;
+    pub async fn on_block_committed(&mut self) -> BlockId {
+        // Remove when events are added
+        #[allow(clippy::never_loop)]
+        loop {
+            let event = self.on_hotstuff_event().await;
+            match event {
+                HotstuffEvent::BlockCommitted { block_id } => return block_id,
+                // _ => {}
+            }
         }
     }
 
-    pub async fn assert_proposal_broadcast(&mut self) -> (TestAddress, ProposalMessage) {
-        let leader = self.get_leader();
-        let msg = self.get_validator_mut(&leader).assert_proposal_broadcast().await;
-        *self.messages_sent.entry(DanMessageType::Proposal).or_default() += 1;
-        (leader, msg)
+    pub fn network(&mut self) -> &mut TestNetwork {
+        &mut self.network
     }
 
-    pub fn get_leader(&self) -> TestAddress {
-        *self
-            .leader_strategy
-            .get_leader(&self.validators.keys().copied().collect(), &BlockId::genesis(), 0)
-    }
-
+    #[allow(dead_code)]
     pub fn get_validator_mut(&mut self, addr: &TestAddress) -> &mut Validator {
         self.validators.get_mut(addr).unwrap()
     }
 
-    pub async fn send_proposal_to_all(&self, from: TestAddress, msg: &ProposalMessage) {
-        for validator in self.validators.values() {
-            validator.send_proposal(from, msg).await;
-        }
+    pub fn get_validator(&self, addr: &TestAddress) -> &Validator {
+        self.validators.get(addr).unwrap()
     }
 
-    pub async fn forward_all_votes_to_leader(&mut self) -> Vec<(TestAddress, VoteMessage)> {
-        let votes = self.assert_votes_sent().await;
-        let leader = self.get_leader();
-        self.get_validator_mut(&leader).send_votes(votes.clone()).await;
-        votes
-    }
-
-    pub async fn assert_votes_sent(&mut self) -> Vec<(TestAddress, VoteMessage)> {
-        let leader = self.get_leader();
-        let votes = join_all(self.validators.values_mut().map(|v| {
-            let address = v.address;
-            v.assert_vote_sent(&leader).map(move |vote| (address, vote))
-        }))
-        .await;
-        *self.messages_sent.entry(DanMessageType::Vote).or_default() += votes.len();
-
-        votes
-    }
-
-    pub fn is_all_transactions_committed(&self) -> bool {
+    pub fn are_all_transactions_committed(&self) -> bool {
         self.validators.values().all(|v| {
             let c = v.uncommitted_transaction_count();
             log::info!("{} has {} unfinalized transactions", v.address, c);
@@ -111,33 +91,18 @@ impl Test {
     pub async fn wait_until_new_pool_count(&self, count: usize) {
         self.wait_all_for_predicate(format!("new pool count to be {}", count), |v| {
             v.state_store
-                .with_read_tx(|tx| tx.new_transaction_pool_get_many_ready(count))
-                .unwrap()
-                .len() >=
+                .with_read_tx(|tx| tx.transaction_pool_count(None, None))
+                .unwrap() >=
                 count
         })
         .await;
     }
 
-    pub async fn wait_for_all_to_have_leaf_height(&self, height: NodeHeight) {
-        let epoch = self.epoch_manager.current_epoch().await.unwrap();
-        self.wait_all_for_predicate(format!("leaf height to be {}", height), |v| {
-            let leaf_height = v
-                .state_store
-                .with_read_tx(|tx| tx.leaf_block_get(epoch))
-                .unwrap()
-                .height;
-
-            leaf_height >= height
-        })
-        .await;
-    }
-
-    pub async fn wait_all_have_at_least_n_transactions_in_pool(&self, n: usize, pool: TransactionPool) {
-        self.wait_all_for_predicate(format!("waiting for {} transactions in pool {:?}", n, pool), |v| {
+    pub async fn wait_all_have_at_least_n_new_transactions_in_pool(&self, n: usize) {
+        self.wait_all_for_predicate(format!("waiting for {} new transaction(s) in pool", n), |v| {
             let pool_count = v
                 .state_store
-                .with_read_tx(|tx| tx.transaction_pools_count(pool))
+                .with_read_tx(|tx| tx.transaction_pool_count(Some(TransactionPoolStage::New), Some(true)))
                 .unwrap();
 
             pool_count >= n
@@ -170,73 +135,119 @@ impl Test {
             tokio::time::sleep(std::time::Duration::from_millis(100)).await;
         }
     }
+
+    pub async fn assert_all_validators_at_same_height(&self) {
+        let epoch = self.epoch_manager.current_epoch().await.unwrap();
+        let committees = self.epoch_manager.all_committees().await;
+        let mut attempts = 0usize;
+        'outer: loop {
+            for committee in committees.values() {
+                let mut heights = self
+                    .validators
+                    .values()
+                    .filter(|vn| committee.members.contains(&vn.address))
+                    .map(|v| {
+                        let height = v
+                            .state_store
+                            .with_read_tx(|tx| Block::get_tip(tx, epoch))
+                            .unwrap()
+                            .height();
+                        (v.address, height)
+                    });
+                let (first_addr, first) = heights.next().unwrap();
+                for (addr, height) in heights {
+                    if first != height && attempts < 5 {
+                        attempts += 1;
+                        // Send this task to the back of the queue and try again after other tasks have executed
+                        // to allow validators to catch up
+                        task::yield_now().await;
+                        continue 'outer;
+                    }
+                    assert_eq!(
+                        first, height,
+                        "Validator {} is at height {} but validator {} is at height {}",
+                        first_addr, first, addr, height
+                    );
+                }
+            }
+            break;
+        }
+    }
+
+    pub async fn assert_clean_shutdown(mut self) {
+        for v in self.validators.values_mut() {
+            v.shutdown.trigger();
+        }
+        for v in self.validators.into_values() {
+            v.handle.await.unwrap();
+        }
+    }
 }
 
 pub struct TestBuilder {
-    addresses: Vec<&'static str>,
+    committees: HashMap<u32, Committee<TestAddress>>,
     sql_address: String,
 }
 
 impl TestBuilder {
     pub fn new() -> Self {
         Self {
-            addresses: vec!["1"],
+            committees: HashMap::new(),
             sql_address: ":memory:".to_string(),
         }
     }
 
+    #[allow(dead_code)]
     pub fn with_sql_url<T: Into<String>>(&mut self, sql_address: T) -> &mut Self {
         self.sql_address = sql_address.into();
         self
     }
 
-    pub fn with_addresses(&mut self, addresses: Vec<&'static str>) -> &mut Self {
-        self.addresses = addresses;
+    pub fn add_committee(&mut self, bucket: u32, addresses: Vec<&'static str>) -> &mut Self {
+        self.committees
+            .insert(bucket, addresses.into_iter().map(TestAddress).collect());
         self
     }
 
-    fn build_validators(
+    async fn build_validators(
         &self,
         leader_strategy: &SelectedIndexLeaderStrategy,
         epoch_manager: &TestEpochManager,
-    ) -> BTreeMap<TestAddress, Validator> {
-        self.addresses
-            .iter()
-            .enumerate()
-            .map(|(i, addr)| {
-                let address = TestAddress(addr);
+    ) -> (Vec<ValidatorChannels>, HashMap<TestAddress, Validator>) {
+        epoch_manager
+            .all_validators()
+            .await
+            .into_iter()
+            .map(|(address, shard)| {
                 let leader_strategy = leader_strategy.clone();
-                let sql_address = self.sql_address.replace("{}", &format!("{}", i + 1));
-                let validator = Validator::builder()
+                let sql_address = self.sql_address.replace("{}", address.0);
+                let (channels, validator) = Validator::builder()
                     .with_sql_url(sql_address)
                     .with_address(address)
-                    .with_epoch_manager(epoch_manager.copy_for(address))
+                    .with_shard(shard)
+                    .with_epoch_manager(epoch_manager.clone_for(address, shard))
                     .with_leader_strategy(leader_strategy)
                     .spawn();
-                (address, validator)
+                (channels, (address, validator))
             })
-            .collect()
+            .unzip()
     }
 
     pub async fn start(&self) -> Test {
         let leader_strategy = SelectedIndexLeaderStrategy::new(0);
         let epoch_manager = TestEpochManager::new();
-        epoch_manager
-            .set_committee(0, self.addresses.clone().into_iter().map(TestAddress).collect())
-            .await;
+        epoch_manager.set_num_committees(self.committees.len() as u32).await;
+        for (bucket, committee) in &self.committees {
+            epoch_manager.add_committee(*bucket, committee.clone()).await;
+        }
+        let (channels, validators) = self.build_validators(&leader_strategy, &epoch_manager).await;
+        let network = spawn_network(channels);
 
         Test {
-            validators: self.build_validators(&leader_strategy, &epoch_manager),
-            leader_strategy,
-            messages_sent: Default::default(),
+            validators,
+            network,
+            _leader_strategy: leader_strategy,
             epoch_manager,
         }
     }
-}
-
-#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
-pub enum DanMessageType {
-    Proposal,
-    Vote,
-    NewView,
 }
---
+++ dan_layer/storage/src/consensus_models/transaction_pool.rs
@@ -0,0 +1,189 @@
+//   Copyright 2023 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Clause
+
+use std::{
+    collections::BTreeSet,
+    fmt::{Display, Formatter},
+    marker::PhantomData,
+    str::FromStr,
+};
+
+use tari_dan_common_types::{committee::CommitteeShard, optional::IsNotFoundError};
+
+use crate::{
+    consensus_models::{Command, QcId, TransactionAtom, TransactionId},
+    StateStore,
+    StateStoreReadTransaction,
+    StateStoreWriteTransaction,
+    StorageError,
+};
+
+#[derive(Debug, Clone, Copy)]
+pub enum TransactionPoolStage {
+    New,
+    LocalPrepared,
+    SomePrepared,
+    AllPrepared,
+}
+
+impl Display for TransactionPoolStage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Debug::fmt(self, f)
+    }
+}
+
+impl FromStr for TransactionPoolStage {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "New" => Ok(TransactionPoolStage::New),
+            "LocalPrepared" => Ok(TransactionPoolStage::LocalPrepared),
+            "SomePrepared" => Ok(TransactionPoolStage::SomePrepared),
+            "AllPrepared" => Ok(TransactionPoolStage::AllPrepared),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct TransactionPool<TStateStore> {
+    _store: PhantomData<TStateStore>,
+}
+
+impl<TStateStore: StateStore> TransactionPool<TStateStore> {
+    pub fn new() -> Self {
+        Self { _store: PhantomData }
+    }
+
+    pub fn get(
+        &self,
+        tx: &mut TStateStore::ReadTransaction<'_>,
+        id: &TransactionId,
+    ) -> Result<TransactionPoolRecord, TransactionPoolError> {
+        let rec = tx.transaction_pool_get(id)?;
+        Ok(rec)
+    }
+
+    pub fn insert(
+        &self,
+        tx: &mut TStateStore::WriteTransaction<'_>,
+        transaction: TransactionAtom,
+    ) -> Result<(), TransactionPoolError> {
+        tx.transaction_pool_insert(transaction, TransactionPoolStage::New, true)?;
+        Ok(())
+    }
+
+    pub fn get_batch(
+        &self,
+        tx: &mut TStateStore::ReadTransaction<'_>,
+        max: usize,
+    ) -> Result<BTreeSet<Command>, TransactionPoolError> {
+        let ready = tx.transaction_pool_get_many_ready(max)?;
+        let commands = ready
+            .into_iter()
+            .map(|t| match t.stage {
+                TransactionPoolStage::New => Command::Prepare(t.transaction),
+                TransactionPoolStage::LocalPrepared => Command::LocalPrepared(t.transaction),
+                TransactionPoolStage::AllPrepared => Command::Accept(t.transaction),
+                // TODO: We move to abort - add some test cases and figure out the best way to handle this
+                TransactionPoolStage::SomePrepared => Command::Accept(t.transaction),
+            })
+            .collect();
+
+        Ok(commands)
+    }
+
+    pub fn has_transactions(&self, tx: &mut TStateStore::ReadTransaction<'_>) -> Result<bool, TransactionPoolError> {
+        let count = self.count(tx)?;
+        Ok(count > 0)
+    }
+
+    pub fn count(&self, tx: &mut TStateStore::ReadTransaction<'_>) -> Result<usize, TransactionPoolError> {
+        let count = tx.transaction_pool_count(None, None)?;
+        Ok(count)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct TransactionPoolRecord {
+    pub transaction: TransactionAtom,
+    pub stage: TransactionPoolStage,
+    pub is_ready: bool,
+}
+
+impl TransactionPoolRecord {
+    pub fn transition<TTx: StateStoreWriteTransaction>(
+        &self,
+        tx: &mut TTx,
+        next_stage: TransactionPoolStage,
+        is_ready: bool,
+    ) -> Result<(), TransactionPoolError> {
+        match ((self.stage, next_stage), is_ready) {
+            ((TransactionPoolStage::New, TransactionPoolStage::LocalPrepared), false) => {
+                tx.transaction_pool_update(&self.transaction.id, None, Some(next_stage), Some(false))?;
+            },
+            ((TransactionPoolStage::LocalPrepared, TransactionPoolStage::LocalPrepared), true) => {
+                tx.transaction_pool_update(&self.transaction.id, None, Some(next_stage), Some(true))?;
+            },
+            ((TransactionPoolStage::LocalPrepared, TransactionPoolStage::AllPrepared), true) => {
+                tx.transaction_pool_update(&self.transaction.id, None, Some(next_stage), Some(true))?;
+            },
+            ((TransactionPoolStage::AllPrepared, TransactionPoolStage::AllPrepared), true) => {
+                tx.transaction_pool_update(&self.transaction.id, None, Some(next_stage), Some(true))?;
+            },
+            _ => {
+                return Err(TransactionPoolError::InvalidTransactionTransition {
+                    from: self.stage,
+                    to: next_stage,
+                    is_ready,
+                })
+            },
+        }
+
+        Ok(())
+    }
+
+    pub fn update_evidence<TTx: StateStoreWriteTransaction>(
+        &mut self,
+        tx: &mut TTx,
+        committee_shard: &CommitteeShard,
+        qc_id: QcId,
+    ) -> Result<(), TransactionPoolError> {
+        let evidence = &mut self.transaction.evidence;
+        for (shard, qcs_mut) in evidence.iter_mut() {
+            if committee_shard.includes_shard(shard) {
+                qcs_mut.push(qc_id);
+            }
+        }
+        tx.transaction_pool_update(&self.transaction.id, Some(evidence), None, None)?;
+
+        Ok(())
+    }
+
+    pub fn remove<TTx: StateStoreWriteTransaction>(&self, tx: &mut TTx) -> Result<(), TransactionPoolError> {
+        tx.transaction_pool_remove(&self.transaction.id)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum TransactionPoolError {
+    #[error("Storage error: {0}")]
+    StorageError(#[from] StorageError),
+    #[error("Invalid transaction transition from {from:?} to {to:?} with is_ready={is_ready}")]
+    InvalidTransactionTransition {
+        from: TransactionPoolStage,
+        to: TransactionPoolStage,
+        is_ready: bool,
+    },
+}
+
+impl IsNotFoundError for TransactionPoolError {
+    fn is_not_found_error(&self) -> bool {
+        match self {
+            TransactionPoolError::StorageError(e) => e.is_not_found_error(),
+            _ => false,
+        }
+    }
+}
