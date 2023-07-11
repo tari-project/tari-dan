5df68cdd71206e23a03ebf59f82727abb9d2f2d4
stringhandler <mikethetike@tari.com>
2023-07-11 15:50:24 +0200

    chore: update to tari 0.51.0-pre.4

--- integration_tests/src/miner.rs
+++ integration_tests/src/miner.rs
@@ -25,16 +25,8 @@ use std::time::Duration;
 use tari_app_grpc::{
     authentication::ClientAuthenticationInterceptor,
     tari_rpc::{
-        pow_algo::PowAlgos,
-        wallet_client::WalletClient,
-        GetCoinbaseRequest,
-        GetCoinbaseResponse,
-        NewBlockTemplate,
-        NewBlockTemplateRequest,
-        NewBlockTemplateResponse,
-        PowAlgo,
-        TransactionKernel,
-        TransactionOutput,
+        pow_algo::PowAlgos, wallet_client::WalletClient, GetCoinbaseRequest, GetCoinbaseResponse, NewBlockTemplate,
+        NewBlockTemplateRequest, NewBlockTemplateResponse, PowAlgo, TransactionKernel, TransactionOutput,
     },
 };
 use tari_base_node_grpc_client::BaseNodeGrpcClient;
@@ -110,7 +102,7 @@ async fn create_block_template_with_coinbase(
     // get the block template from the base node
     let template_req = NewBlockTemplateRequest {
         algo: Some(PowAlgo {
-            pow_algo: PowAlgos::Sha3.into(),
+            pow_algo: PowAlgos::Sha3x.into(),
         }),
         max_weight: 0,
     };
