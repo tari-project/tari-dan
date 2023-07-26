4527548c4c0f25b89227aaef00b56929c4bc9cab
Erika <87762061+NovaT82@users.noreply.github.com>
2023-07-26 12:24:22 +0200

    feat: token permissions and ui updates (#628)
    Description
    ---
    Adds the permissions field to the access tokens table.
    Also adds a couple of ui updates for Accounts, AccountDetails and Keys.
    
    Motivation and Context
    ---
    UI Improvements
    
    How Has This Been Tested?
    ---
    Manually
    
    What process can a PR reviewer use to test or verify this change?
    ---
    There will now be a dropdown with permissions in the Access Tokens
    table.
    
    Breaking Changes
    ---
    x
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify

--- applications/tari_dan_wallet_web_ui/src/routes/Wallet/Components/Keys.tsx
+++ applications/tari_dan_wallet_web_ui/src/routes/Wallet/Components/Keys.tsx
@@ -33,23 +33,24 @@ import { BoxHeading2 } from '../../../Components/StyledComponents';
 import AddIcon from '@mui/icons-material/Add';
 import Fade from '@mui/material/Fade';
 import { Form } from 'react-router-dom';
-import TextField from '@mui/material/TextField/TextField';
 import Button from '@mui/material/Button/Button';
+import { DataTableCell } from '../../../Components/StyledComponents';
+import { Alert } from '@mui/material';
 
-function Key(key:any,setActive:any) {
+function Key(key: any, setActive: any) {
   return (
     <TableRow key={key[0]}>
-      <TableCell>
-        {key[0]}
-      </TableCell>
-      <TableCell>
-        {key[1]}
-      </TableCell>
-      <TableCell>
-        {key[2] ?<b>Active</b> : <div onClick={() => setActive(key[0])}>Activate</div>}
-      </TableCell>
+      <DataTableCell>{key[0]}</DataTableCell>
+      <DataTableCell>{key[1]}</DataTableCell>
+      <DataTableCell>
+        {key[2] ? (
+          <b>Active</b>
+        ) : (
+          <div onClick={() => setActive(key[0])}>Activate</div>
+        )}
+      </DataTableCell>
     </TableRow>
-  )
+  );
 }
 
 function Keys() {
@@ -66,35 +67,40 @@ function Keys() {
   };
   const loadKeys = () => {
     keysList()
-    .then((response) => {
-      console.log(response);
-      setState(response);
-      setError(undefined);
-    })
-    .catch((err) => {
-      setError(err && err.message ? err.message : `Unknown error: ${JSON.stringify(err)}`);
-    });
+      .then((response) => {
+        console.log(response);
+        setState(response);
+        setError(undefined);
+      })
+      .catch((err) => {
+        setError(
+          err && err.message
+            ? err.message
+            : `Unknown error: ${JSON.stringify(err)}`
+        );
+      });
   };
-  const setActive = (index:number) => {
+  const setActive = (index: number) => {
     keysSetActive(index).then((response) => {
       loadKeys();
-    })
-    console.log('click')
+    });
+    console.log('click');
   };
   const onSubmitAddKey = () => {
-    keysCreate().then((response)=>{
-      loadKeys()
-    })
+    keysCreate().then((response) => {
+      loadKeys();
+    });
     setFormState({ publicKey: '', address: '' });
     setShowAddKeyDialog(false);
   };
   useEffect(() => {
-    loadKeys()
+    loadKeys();
   }, []);
   if (error) {
-    return <Error component="Keys" message={error} />;
+    return <Alert severity="error">{error}</Alert>;
   }
-  return (<>
+  return (
+    <>
       <BoxHeading2>
         {showKeyDialog && (
           <Fade in={showKeyDialog}>
@@ -124,26 +130,21 @@ function Keys() {
             </div>
           </Fade>
         )}
-      </BoxHeading2>    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              Index
-            </TableCell>
-            <TableCell>
-              Public key
-            </TableCell>
-            <TableCell>
-              Active
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {state && state.keys.map((key:any)=>Key(key, setActive))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      </BoxHeading2>{' '}
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Index</TableCell>
+              <TableCell>Public key</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {state && state.keys.map((key: any) => Key(key, setActive))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
