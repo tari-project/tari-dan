e09d05b49c48ca94c09d3244434cf954c08ec2fa
stringhandler <stringhandler@gmail.com>
2023-09-06 13:20:58 +0200

    fix: update trigger

--- dan_layer/state_store_sqlite/migrations/2023-07-20-192955_add_tracking_triggers/up.sql
+++ dan_layer/state_store_sqlite/migrations/2023-07-20-192955_add_tracking_triggers/up.sql
@@ -8,11 +8,16 @@ CREATE TABLE transaction_pool_history
     id               integer   not null,
     transaction_id   text      not null,
     involved_shards  text      not null,
-    overall_decision text      not null,
+    original_decision text      not null,
+    local_decision  text      null,
+    remote_decision  text      null,
     evidence         text      not null,
-    fee              bigint    not null,
+    transaction_fee              bigint    not null,
+    leader_fee              bigint    not null,
     stage            text      not null,
+    pending_stage            text      null,
     is_ready         boolean   not null,
+    updated_at    timestamp NOT NULL,
     created_at       timestamp NOT NULL,
     change_time      DATETIME DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))
 );
@@ -22,21 +27,34 @@ CREATE TRIGGER copy_transaction_pool_history
     ON transaction_pool
     FOR EACH ROW
 BEGIN
-    INSERT INTO transaction_pool_history (id, transaction_id,
+    INSERT INTO transaction_pool_history (id,
+                                          transaction_id,
                                           involved_shards,
-                                          overall_decision,
+                                          original_decision,
+                                          local_decision,
+                                          remote_decision,
                                           evidence,
-                                          fee,
+                                          transaction_fee,
+                                          leader_fee,
                                           stage,
+                                          pending_stage,
                                           is_ready,
+                                          updated_at,
                                           created_at)
-    VALUES (OLD.id,
-            OLD.transaction_id,
-            OLD.involved_shards,
-            OLD.overall_decision,
-            OLD.evidence,
-            OLD.fee,
-            OLD.stage,
-            OLD.is_ready,
-            OLD.created_at);
+    VALUES  (
+             OLD.id,
+             OLD.transaction_id,
+             OLD.involved_shards,
+             OLD.original_decision,
+             OLD.local_decision,
+             OLD.remote_decision,
+             OLD.evidence,
+             OLD.transaction_fee,
+             OLD.leader_fee,
+             OLD.stage,
+             OLD.pending_stage,
+             OLD.is_ready,
+             OLD.updated_at,
+             OLD.created_at
+             );
 END;
