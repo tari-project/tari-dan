fbbf486a8f4ccda1d3405279f520763941ad73aa
Cifko <gcifko@gmail.com>
2023-09-07 14:35:01 +0200

    fix

--- applications/tari_validator_node/src/dan_node.rs
+++ applications/tari_validator_node/src/dan_node.rs
@@ -165,11 +165,13 @@ impl DanNode {
                     }
                 }
 
-        info!(target: LOG_TARGET, "🏁 Removing {} finalized transaction(s) from mempool", committed_transactions.len());
-        for tx_id in committed_transactions {
-            if let Err(err) = self.services.mempool.remove_transaction(tx_id).await {
-                error!(target: LOG_TARGET, "Failed to remove transaction from mempool: {}", err);
-            }
+                info!(target: LOG_TARGET, "🏁 Removing {} finalized transaction(s) from mempool", committed_transactions.len());
+                for tx_id in committed_transactions {
+                    if let Err(err) = self.services.mempool.remove_transaction(tx_id).await {
+                        error!(target: LOG_TARGET, "Failed to remove transaction from mempool: {}", err);
+                    }
+                }
+            },
         }
     }
 
