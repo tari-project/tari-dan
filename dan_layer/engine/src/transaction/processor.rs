318ae7a9dc7380d63727261595f1b1eddd898141
Martin Stefcek <gcifko@gmail.com>
2023-10-20 10:49:15 +0200

    shorten fn

--- dan_layer/engine/src/transaction/processor.rs
+++ dan_layer/engine/src/transaction/processor.rs
@@ -151,21 +151,15 @@ impl<TTemplateProvider: TemplateProvider<Template = LoadedTemplate> + 'static> T
                 } = runtime.interface().finalize()?;
 
                 if !fee_receipt.is_paid_in_full() {
+                    let reason = RejectReason::FeesNotPaid(format!(
+                        "Required fees {} but {} paid",
+                        fee_receipt.total_fees_charged(),
+                        fee_receipt.total_fees_paid()
+                    ));
                     finalized.result = if let Some(accept) = finalized.result.accept() {
-                        TransactionResult::AcceptFeeRejectRest(
-                            accept.clone(),
-                            RejectReason::FeesNotPaid(format!(
-                                "Required fees {} but {} paid",
-                                fee_receipt.total_fees_charged(),
-                                fee_receipt.total_fees_paid()
-                            )),
-                        )
+                        TransactionResult::AcceptFeeRejectRest(accept.clone(), reason)
                     } else {
-                        TransactionResult::Reject(RejectReason::FeesNotPaid(format!(
-                            "Required fees {} but {} paid",
-                            fee_receipt.total_fees_charged(),
-                            fee_receipt.total_fees_paid()
-                        )))
+                        TransactionResult::Reject(reason)
                     };
                     return Ok(ExecuteResult {
                         finalize: finalized,
