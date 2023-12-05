2339d4b8f4e75bca0d75aeddf15eebcfa1af7e5d
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2023-12-05 16:58:14 +0000

    constants

--- dan_layer/template_lib/src/constants.rs
+++ dan_layer/template_lib/src/constants.rs
@@ -13,5 +13,8 @@ pub const PUBLIC_IDENTITY_RESOURCE_ADDRESS: ResourceAddress = ResourceAddress::n
     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]));
 
+/// The Tari network native resource address, used for paying network fees
 pub const CONFIDENTIAL_TARI_RESOURCE_ADDRESS: ResourceAddress = ResourceAddress::new(Hash::from_array([1u8; 32]));
+
+/// Shorthand version of the `CONFIDENTIAL_TARI_RESOURCE_ADDRESS` constant
 pub const XTR2: ResourceAddress = CONFIDENTIAL_TARI_RESOURCE_ADDRESS;
