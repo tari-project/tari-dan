5d615c56beaecffbfd0b16f1541709e292e25aca
Stan Bondi <sdbondi@users.noreply.github.com>
2024-02-16 15:44:21 +0400

    remove unused build-deps

--- dan_layer/common_types/src/validator_metadata.rs
+++ dan_layer/common_types/src/validator_metadata.rs
@@ -1,13 +1,11 @@
 //   Copyright 2022 The Tari Project
 //   SPDX-License-Identifier: BSD-3-Clause
 
-use blake2::{digest::consts::U32, Blake2b};
 use serde::{Deserialize, Serialize};
 use tari_common::configuration::Network;
 use tari_common_types::types::{FixedHash, PublicKey, Signature};
-use tari_crypto::{hash_domain, hashing::DomainSeparation};
-use tari_engine_types::hashing::TariHasher32;
-use tari_hash_domains::TariEngineHashDomain;
+use tari_crypto::hash_domain;
+use tari_engine_types::base_layer_hashing::TariBaseLayerHasher32;
 
 // TODO: add to tari_hash_domains
 hash_domain!(TransactionHashDomain, "com.tari.base_layer.core.transactions", 0);
@@ -32,16 +30,13 @@ impl ValidatorMetadata {
 }
 
 pub fn vn_node_hash(network: Network, public_key: &PublicKey, substate_address: &SubstateAddress) -> FixedHash {
-    let mut digest = Blake2b::<U32>::default();
-    TariEngineHashDomain::add_domain_separation_tag(&mut digest, &format!("validator_node.n{}", network.as_byte()));
-    // TODO: TariHasher32 is the same as the consensus hasher in tari_core. The consensus hasher should be part of the
-    // common hashing crate, currently called tari_hash_domains. Should rename it to tari_hasher/tari_hashing and
-    // include the consensus hasher. This is done to remove the dependency on tari_core which has a bunch of
+    // TODO: TariBaseLayerHasher32 is the same as the consensus hasher in tari_core. The consensus hasher should be part
+    // of the common hashing crate, currently called tari_hash_domains. Should rename it to tari_hasher/tari_hashing
+    // and include the consensus hasher. This is done to remove the dependency on tari_core which has a bunch of
     // dependencies e.g. tari_comms, dht etc. "Type" crates should always have minimal dependencies.
-    TariHasher32::from_digest(digest)
+    TariBaseLayerHasher32::new_with_label::<TransactionHashDomain>(network, "validator_node")
         .chain(public_key)
         .chain(&substate_address.0)
         .result()
-        .into_array()
         .into()
 }
