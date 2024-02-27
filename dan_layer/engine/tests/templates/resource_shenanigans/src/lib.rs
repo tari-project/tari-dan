2c91463ed956f919da0e3bd31e31527b83856a01
Stan Bondi <sdbondi@users.noreply.github.com>
2024-02-27 12:43:44 +0400

    fix(engine): prevent creation of mismatched resource type bucket (#940)
    Description
    ---
    fix(engine): add resource type check when processing mint args
    fix(engine): improve error for resource type mismatch
    fix(engine): erroneous dangling resource error when creating an empty
    vault from a resource
    test(engine): add previously failing test for minting a different
    resource type
    
    Motivation and Context
    ---
    By specifying mismatching MintArgs you were previously able to create a
    bucket for a resource type that did not match the resource being
    operated on.
    
    This test exposed another bug which is also tested in the new test. This
    occurs when you create an empty vault from a new resource, the engine
    fails with a dangling resource error because it does not recognise that
    the new empty resource is contained within the new empty vault.
    
    How Has This Been Tested?
    ---
    A new failing test, that passes after changes
    
    What process can a PR reviewer use to test or verify this change?
    ---
    Run engine tests
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify

---
+++ dan_layer/engine/tests/templates/resource_shenanigans/src/lib.rs
@@ -0,0 +1,29 @@
+//   Copyright 2023 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Clause
+
+use tari_template_lib::prelude::*;
+
+#[template]
+mod template {
+    use super::*;
+
+    pub struct Shenanigans {
+        vault: Vault,
+    }
+
+    impl Shenanigans {
+        pub fn new() -> Component<Shenanigans> {
+            let resource = ResourceBuilder::non_fungible().mintable(AccessRule::AllowAll).build();
+
+            Component::new(Self {
+                vault: Vault::new_empty(resource),
+            })
+            .with_access_rules(AccessRules::allow_all())
+            .create()
+        }
+
+        pub fn mint_different_resource_type(&mut self) -> Bucket {
+            ResourceManager::get(self.vault.resource_address()).mint_fungible(Amount(1000))
+        }
+    }
+}
