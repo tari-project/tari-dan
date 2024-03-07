944f663a32602a4682d1ac56de1a032cafd2b834
Stan Bondi <sdbondi@users.noreply.github.com>
2024-03-07 13:38:57 +0400

    fix create account nft call

--- utilities/transaction_generator/src/transaction_builders/free_coins.rs
+++ utilities/transaction_generator/src/transaction_builders/free_coins.rs
@@ -2,16 +2,17 @@
 //   SPDX-License-Identifier: BSD-3-Clause
 
 use rand::rngs::OsRng;
-use tari_crypto::{keys::PublicKey, ristretto::RistrettoPublicKey, tari_utilities::ByteArray};
-use tari_engine_types::{component::new_component_address_from_parts, instruction::Instruction};
+use tari_crypto::{keys::PublicKey, ristretto::RistrettoPublicKey};
+use tari_engine_types::{component::new_account_address_from_parts, instruction::Instruction};
 use tari_template_builtin::ACCOUNT_TEMPLATE_ADDRESS;
-use tari_template_lib::{args, crypto::RistrettoPublicKeyBytes, models::Amount};
+use tari_template_lib::{args, models::Amount};
 use tari_transaction::Transaction;
 
 pub fn builder(_: u64) -> Transaction {
     let (signer_secret_key, signer_public_key) = RistrettoPublicKey::random_keypair(&mut OsRng);
 
-    let owner_pk = RistrettoPublicKeyBytes::from_bytes(signer_public_key.as_bytes()).unwrap();
+    let account_address = new_account_address_from_parts(&ACCOUNT_TEMPLATE_ADDRESS, &signer_public_key);
+
     Transaction::builder()
         .with_fee_instructions_builder(|builder| {
             builder
@@ -21,11 +22,7 @@ pub fn builder(_: u64) -> Transaction {
                 })
                 .put_last_instruction_output_on_workspace(b"free_coins")
                 .create_account_with_bucket(signer_public_key, "free_coins")
-                .call_method(
-                    new_component_address_from_parts(&ACCOUNT_TEMPLATE_ADDRESS, &owner_pk.into_array().into()),
-                    "pay_fee",
-                    args![Amount(1000)],
-                )
+                .call_method(account_address, "pay_fee", args![Amount(1000)])
         })
         .sign(&signer_secret_key)
         .build()
