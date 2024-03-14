f8478848aa15077c899436a1172ef766daca03e8
Miguel Naveira <47919901+mrnaveira@users.noreply.github.com>
2024-03-14 04:40:39 +0000

    chore: update TypeScript bindings (#973)
    Description
    ---
    Updated TypeScript bindings by running `npm run build` inside the
    `/bindings` folder
    
    Motivation and Context
    ---
    Recent changes in the project made the bindings obsolete, this PR
    updates them
    
    How Has This Been Tested?
    ---
    Does not apply
    
    What process can a PR reviewer use to test or verify this change?
    ---
    Does not apply
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify

--- bindings/src/types/validator-node-client/GetEpochManagerStatsResponse.ts
+++ bindings/src/types/validator-node-client/GetEpochManagerStatsResponse.ts
@@ -5,6 +5,7 @@ import type { Epoch } from "../Epoch";
 export interface GetEpochManagerStatsResponse {
   current_epoch: Epoch;
   current_block_height: number;
+  current_block_hash: string;
   is_valid: boolean;
   committee_shard: CommitteeShard | null;
 }
