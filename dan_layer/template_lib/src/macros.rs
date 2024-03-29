1cdb1ab0b3d857321a0230f8c2907f8ca261445f
Stan Bondi <sdbondi@users.noreply.github.com>
2024-03-29 09:00:40 +0400

    feat(template_lib): adds log macros (#992)
    Description
    ---
    feat(template_lib): adds log macros
    
    Motivation and Context
    ---
    
    Adds log macros to template lib. `info!`, `warn!`, `error!` will call
    `engine().emit_log(LogLevel::XXXX, format!(...))`
    
    How Has This Been Tested?
    ---
    emit_log is already tested
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify

--- dan_layer/template_lib/src/macros.rs
+++ dan_layer/template_lib/src/macros.rs
@@ -15,3 +15,47 @@ macro_rules! debug {
         $crate::macros::call_debug(format!($fmt, $($args)*))
     };
 }
+
+/// Macro for emitting log messages from inside templates
+#[macro_export]
+macro_rules! log {
+    ($lvl:expr, $fmt:expr) => {
+        $crate::engine::engine().emit_log($lvl, $fmt)
+    };
+    ($lvl:expr, $fmt:expr, $($args:tt)*) => {
+        $crate::engine::engine().emit_log($lvl, format!($fmt, $($args)*))
+    };
+}
+
+/// Macro for emitting log messages from inside templates
+#[macro_export]
+macro_rules! info {
+    ($fmt:expr) => {
+        $crate::macros::log!($crate::args::LogLevel::Info, $fmt)
+    };
+    ($fmt:expr, $($args:tt)*) => {
+        $crate::macros::log!($crate::args::LogLevel::Info, $fmt, $($args)*)
+    };
+}
+
+/// Macro for emitting warn log messages from inside templates
+#[macro_export]
+macro_rules! warn {
+    ($fmt:expr) => {
+        $crate::macros::log!($crate::args::LogLevel::Warn, $fmt)
+    };
+    ($fmt:expr, $($args:tt)*) => {
+        $crate::macros::log!($crate::args::LogLevel::Warn, $fmt, $($args)*)
+    };
+}
+
+/// Macro for emitting error log messages from inside templates
+#[macro_export]
+macro_rules! error {
+    ($fmt:expr) => {
+        $crate::macros::log!($crate::args::LogLevel::Error, $fmt)
+    };
+    ($fmt:expr, $($args:tt)*) => {
+        $crate::macros::log!($crate::args::LogLevel::Error, $fmt, $($args)*)
+    };
+}
