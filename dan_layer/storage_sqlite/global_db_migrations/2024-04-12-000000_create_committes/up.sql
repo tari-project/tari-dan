d52c820e87944a3a78d4f7830fe5726c34989fe9
Stan Bondi <sdbondi@users.noreply.github.com>
2024-04-23 16:15:31 +0400

    remove fk from committees table

--- dan_layer/storage_sqlite/global_db_migrations/2024-04-12-000000_create_committes/up.sql
+++ dan_layer/storage_sqlite/global_db_migrations/2024-04-12-000000_create_committes/up.sql
@@ -1,9 +1,13 @@
-CREATE TABLE committees (
+CREATE TABLE committees
+(
     id               INTEGER PRIMARY KEY autoincrement NOT NULL,
     public_key       BLOB,
-    epoch            BIGINT NOT NULL,
-    committee_bucket BIGINT NOT NULL,
-    FOREIGN KEY (public_key) REFERENCES validator_nodes (public_key)
+    epoch            BIGINT                            NOT NULL,
+    committee_bucket BIGINT                            NOT NULL
+--  This is strange: public key always errors with foreign key mismatches on insert
+--  even though it exists in the validator node table. Tested with clean db etc.
+--  Might be because of the BLOB type and/or some bug in diesel.
+--     FOREIGN KEY (public_key) REFERENCES validator_nodes (public_key)
 );
 
 CREATE INDEX committees_epoch_index ON committees (epoch);
