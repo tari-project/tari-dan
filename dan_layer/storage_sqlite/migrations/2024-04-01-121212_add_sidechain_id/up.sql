94642a32dab97718f958d40f0da3795f3cfccd9e
stringhandler <stringhandler@protonmail.com>
2024-05-06 11:07:39 +0200

    fix error, sql

--- dan_layer/storage_sqlite/migrations/2024-04-01-121212_add_sidechain_id/up.sql
+++ dan_layer/storage_sqlite/migrations/2024-04-01-121212_add_sidechain_id/up.sql
@@ -1,3 +1,7 @@
 -- Your SQL goes here
 ALTER TABLE validator_nodes
     ADD COLUMN sidechain_id BLOB NULL;
+
+drop index validator_nodes_public_key_uniq_idx;
+
+create unique index validator_nodes_public_key_uniq_idx on validator_nodes (public_key, sidechain_id);
