96a13cf7febcb168d903c6f5f96ee1ca7fee0cad
stringhandler <stringhandler@protonmail.com>
2024-05-15 16:00:15 +0200

    undo one ts

--- bindings/src/types/ResourceContainer.ts
+++ bindings/src/types/ResourceContainer.ts
@@ -6,10 +6,19 @@ import type { ResourceAddress } from "./ResourceAddress";
 export type ResourceContainer =
   | { Fungible: { address: ResourceAddress; amount: Amount; locked_amount: Amount } }
   | {
-      NonFungible: {
-        address: ResourceAddress;
-        token_ids: Array<NonFungibleId>;
-        locked_token_ids: Array<NonFungibleId>;
-      };
-    }
-  | { Confidential: { address: ResourceAddress; revealed_amount: Amount; locked_revealed_amount: Amount } };
+  NonFungible: {
+    address: ResourceAddress;
+    token_ids: Array<NonFungibleId>;
+    locked_token_ids: Array<NonFungibleId>;
+  };
+}
+  | {
+  Confidential: {
+    address: ResourceAddress;
+    commitments: Record<string, ConfidentialOutput>;
+    revealed_amount: Amount;
+    locked_commitments: Record<string, ConfidentialOutput>;
+    locked_revealed_amount: Amount;
+  };
+};
+import { ConfidentialOutput } from "./ConfidentialOutput";
