c04ec57d8a9b8c9adf9f6bb3b88f679f7b682706
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2024-07-30 16:02:47 +0100

    create test utils

--- dan_layer/engine/tests/asserts.rs
+++ dan_layer/engine/tests/asserts.rs
@@ -3,13 +3,24 @@
 
 use std::vec;
 
-use tari_template_lib::{args, models::{Amount, ComponentAddress}};
+use tari_crypto::ristretto::RistrettoSecretKey;
+use tari_template_lib::{args, models::{Amount, ComponentAddress, NonFungibleAddress, ResourceAddress}};
 use tari_template_test_tooling::TemplateTest;
 use tari_transaction::{Instruction, Transaction};
 
-#[test]
-fn successful_assert() {
-    let mut template_test = TemplateTest::new(Vec::<&str>::new());
+const FAUCET_WITHDRAWAL_AMOUNT: Amount = Amount::new(1000);
+
+struct AssertTest {
+    template_test: TemplateTest,
+    faucet_component: ComponentAddress,
+    faucet_resource: ResourceAddress,
+    account: ComponentAddress,
+    account_proof: NonFungibleAddress,
+    account_key: RistrettoSecretKey,
+}
+
+fn setup() -> AssertTest {
+    let mut template_test = TemplateTest::new(vec!["tests/templates/tariswap"]);
 
     let faucet_template = template_test.get_template_address("TestFaucet");
 
@@ -36,19 +47,36 @@ fn successful_assert() {
         .unwrap();
 
     // Create user account to receive faucet tokens
-    let (user_account, user_proof, user_key) = template_test.create_funded_account();
-    let min_amount = Amount::new(1000);
+    let (account, account_proof, account_key) = template_test.create_funded_account();
 
-    template_test.execute_expect_success(
+    AssertTest {
+        template_test,
+        faucet_component,
+        faucet_resource,
+        account,
+        account_proof,
+        account_key
+    }
+}
+
+#[test]
+fn successful_assert() {
+    let mut test: AssertTest = setup();
+
+    let min_amount = FAUCET_WITHDRAWAL_AMOUNT;
+
+    test.template_test.execute_expect_success(
         Transaction::builder()
-            .call_method(faucet_component, "take_free_coins", args![])
+            .call_method(test.faucet_component, "take_free_coins", args![])
             .put_last_instruction_output_on_workspace("free_coins")
-            .assert_bucket_contains("free_coins", faucet_resource, min_amount)
-            .call_method(user_account, "deposit", args![Workspace("free_coins")])
-            .sign(&user_key)
+            .assert_bucket_contains("free_coins", test.faucet_resource, min_amount)
+            .call_method(test.account, "deposit", args![Workspace("free_coins")])
+            .sign(&test.account_key)
             .build(),
         // Because we deny_all on deposits, we need to supply the owner proof to be able to deposit the initial
         // tokens into the new vaults
-        vec![user_proof.clone()],
+        vec![test.account_proof.clone()],
     );
 }
+
+
