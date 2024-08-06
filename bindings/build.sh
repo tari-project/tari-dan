979714343d98c888e88f1e11504ba7721dec4db2
Maciej Kozuszek <mackozuszek@gmail.com>
2024-08-06 11:09:20 +0200

    Move ts files to src directory

--- bindings/build.sh
+++ bindings/build.sh
@@ -3,57 +3,55 @@
 
 set -e
 
-DIRECTORY_PATH="./src/types" # replace with your directory path
-HELPERS_PATH="./src/helpers" # replace with your directory path
-INDEX_FILE="./index.ts"
-npx shx rm -rf $DIRECTORY_PATH/*
+SOURCE_PATH="./src"
+TYPES_DIR="types"
+HELPERS_DIR="helpers"
+INDEX_FILE="index.ts"
+npx shx rm -rf $SOURCE_PATH/$TYPES_DIR
+npx shx rm -rf $SOURCE_PATH/$INDEX_FILE
 
 cargo test --workspace --exclude integration_tests export_bindings --features ts
 npx shx mv ../dan_layer/bindings/src/types/* ./src/types/
 npx shx rm -rf ../dan_layer/bindings/
 
-# Remove the index file if it exists
-if [ -f "$INDEX_FILE" ]; then
-  npx shx rm "$INDEX_FILE"
-fi
-
 # Add the license header
-echo "//   Copyright 2023 The Tari Project" >> $INDEX_FILE
-echo "//   SPDX-License-Identifier: BSD-3-Clause" >> $INDEX_FILE
-echo "" >> $INDEX_FILE
+echo "//   Copyright 2023 The Tari Project" >> $SOURCE_PATH/$INDEX_FILE
+echo "//   SPDX-License-Identifier: BSD-3-Clause" >> $SOURCE_PATH/$INDEX_FILE
+echo "" >> $SOURCE_PATH/$INDEX_FILE
 
+cd ./src
 # Generate the index file
-for file in $(find $DIRECTORY_PATH -name "*.ts" -maxdepth 1 | sort); do
+for file in $(find $TYPES_DIR -name "*.ts" -maxdepth 1 | sort); do
   MODULE_NAME="${file%.*}"
-  echo "export * from '$MODULE_NAME';" >> $INDEX_FILE
+  echo "export * from './$MODULE_NAME';" >> $INDEX_FILE
 done
 
-for dir in $(find $DIRECTORY_PATH -mindepth 1 -maxdepth 1 -type d | sort); do
+for dir in $(find $TYPES_DIR -mindepth 1 -maxdepth 1 -type d | sort); do
   index_file="$(basename $dir).ts"
   if [ -f "$index_file" ]; then
     npx shx rm "$index_file"
   fi
-  echo "//   Copyright 2023 The Tari Project" >> $index_file
-  echo "//   SPDX-License-Identifier: BSD-3-Clause" >> $index_file
-  echo "" >> $index_file
+  echo "//   Copyright 2023 The Tari Project" >> "$index_file"
+  echo "//   SPDX-License-Identifier: BSD-3-Clause" >> "$index_file"
+  echo "" >> "$index_file"
   for file in $(find $dir -name "*.ts" -maxdepth 1); do
-    # FILE_NAME=$(basename $file)
     MODULE_NAME="${file%.*}"
-    echo "export * from '$MODULE_NAME';" >> $index_file
+    echo "export * from './$MODULE_NAME';" >> "$index_file"
   done
+  # echo "export * from './$(basename $dir)';" >> $INDEX_FILE // TODO: solve namespace conflict between validator-node-client and tari-indexer-client
 done
 
 # Add helpers
-for file in $(find $HELPERS_PATH -name "*.ts" | sort); do
+for file in $(find $HELPERS_DIR -name "*.ts" | sort); do
   FILE_NAME=$(basename $file)
   if [ "$FILE_NAME" != "index.ts" ]; then
     MODULE_NAME="${FILE_NAME%.*}"
-    echo "export * from '$HELPERS_PATH/$MODULE_NAME';" >> $INDEX_FILE
+    echo "export * from './$HELPERS_DIR/$MODULE_NAME';" >> $INDEX_FILE
   fi
 done
 
 # This is temporary solution to the problem of 'Commitment' not being exported, and we have to do manual types in the
 # code for BTreeMap<Commitment, ConfidentialOutput>. Because of this the ConfidentialOutput type is not imported.
-echo "import { ConfidentialOutput } from './ConfidentialOutput';" >> $DIRECTORY_PATH/ResourceContainer.ts
+echo "import { ConfidentialOutput } from './ConfidentialOutput';" >> $TYPES_DIR/ResourceContainer.ts
 
 npx prettier --write "./**/*.{ts,tsx,css,json}" --log-level=warn
---
+++ bindings/src/tari-indexer-client.ts
@@ -0,0 +1,37 @@
+//   Copyright 2023 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Clause
+
+export * from "./types/tari-indexer-client/GetAllVnsRequest";
+export * from "./types/tari-indexer-client/GetSubstateRequest";
+export * from "./types/tari-indexer-client/Connection";
+export * from "./types/tari-indexer-client/GetTemplateDefinitionResponse";
+export * from "./types/tari-indexer-client/GetNonFungibleCountRequest";
+export * from "./types/tari-indexer-client/GetSubstateResponse";
+export * from "./types/tari-indexer-client/GetNonFungiblesResponse";
+export * from "./types/tari-indexer-client/GetAllVnsResponse";
+export * from "./types/tari-indexer-client/InspectSubstateRequest";
+export * from "./types/tari-indexer-client/ListSubstateItem";
+export * from "./types/tari-indexer-client/ListSubstatesResponse";
+export * from "./types/tari-indexer-client/GetTransactionResultRequest";
+export * from "./types/tari-indexer-client/SubmitTransactionRequest";
+export * from "./types/tari-indexer-client/GetNonFungiblesRequest";
+export * from "./types/tari-indexer-client/GetTransactionResultResponse";
+export * from "./types/tari-indexer-client/ListSubstatesRequest";
+export * from "./types/tari-indexer-client/GetIdentityResponse";
+export * from "./types/tari-indexer-client/GetRelatedTransactionsResponse";
+export * from "./types/tari-indexer-client/SubmitTransactionResponse";
+export * from "./types/tari-indexer-client/GetEpochManagerStatsResponse";
+export * from "./types/tari-indexer-client/GetRelatedTransactionsRequest";
+export * from "./types/tari-indexer-client/ConnectionDirection";
+export * from "./types/tari-indexer-client/InspectSubstateResponse";
+export * from "./types/tari-indexer-client/AddPeerRequest";
+export * from "./types/tari-indexer-client/NonFungibleSubstate";
+export * from "./types/tari-indexer-client/GetNonFungibleCountResponse";
+export * from "./types/tari-indexer-client/GetCommsStatsResponse";
+export * from "./types/tari-indexer-client/GetTemplateDefinitionRequest";
+export * from "./types/tari-indexer-client/ListTemplatesResponse";
+export * from "./types/tari-indexer-client/IndexerTransactionFinalizedResult";
+export * from "./types/tari-indexer-client/ListTemplatesRequest";
+export * from "./types/tari-indexer-client/GetNonFungibleCollectionsResponse";
+export * from "./types/tari-indexer-client/AddPeerResponse";
+export * from "./types/tari-indexer-client/GetConnectionsResponse";
