4267d58f39fc7d9e3e79e36c3dd00ba1a1d54df8
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2024-09-18 21:07:30 +0100

    gossip subscribe/unsuscribe logic

--- applications/tari_validator_node/src/consensus/gossip.rs
+++ applications/tari_validator_node/src/consensus/gossip.rs
@@ -10,7 +10,7 @@ use log::*;
 use tari_consensus::messages::HotstuffMessage;
 use tari_dan_common_types::{Epoch, PeerAddress, ShardGroup, SubstateAddress};
 use tari_dan_p2p::{proto::{self, network::Message}, DanMessage};
-use tari_epoch_manager::{base_layer::EpochManagerHandle, EpochManagerError, EpochManagerReader};
+use tari_epoch_manager::{base_layer::EpochManagerHandle, EpochManagerError, EpochManagerEvent, EpochManagerReader};
 use tari_networking::NetworkingError;
 
 use crate::p2p::services::{mempool::MempoolError, messaging::Gossip};
@@ -33,31 +33,67 @@ pub enum ConsensusGossipError {
 }
 
 #[derive(Debug)]
-pub(super) struct ConsensusGossip<TAddr> {
+pub(super) struct ConsensusGossipService<TAddr> {
     epoch_manager: EpochManagerHandle<TAddr>,
     gossip: Gossip,
+    is_subscribed: Option<ShardGroup>,
 }
 
-impl ConsensusGossip<PeerAddress> {
+impl ConsensusGossipService<PeerAddress> {
     pub fn new(epoch_manager: EpochManagerHandle<PeerAddress>, outbound: Gossip) -> Self {
         Self {
             epoch_manager,
             gossip: outbound,
+            is_subscribed: None,
         }
     }
 
-    pub async fn next_message(&mut self) -> Option<Result<(PeerAddress, HotstuffMessage), ConsensusGossipError>> {
+    pub async fn run(mut self) -> anyhow::Result<()> {
+        let mut events = self.epoch_manager.subscribe().await?;
+
+        loop {
+            tokio::select! {
+                Some(result) = self.next_message() => {
+                    if let Err(e) = self.handle_new_message(result).await {
+                        warn!(target: LOG_TARGET, "Consensus gossip service rejected message: {}", e);
+                    }
+                }
+                Ok(event) = events.recv() => {
+                    if let EpochManagerEvent::EpochChanged(epoch) = event {
+                        if self.epoch_manager.is_this_validator_registered_for_epoch(epoch).await?{
+                            info!(target: LOG_TARGET, "Consensus gossip service subscribing messages for epoch {}", epoch);
+                            self.subscribe(epoch).await?;
+
+                            // TODO: unsubscribe older epoch shards?
+                        }
+                    }
+                },
+
+                else => {
+                    info!(target: LOG_TARGET, "Consensus gossip service shutting down");
+                    break;
+                }
+            }
+        }
+
+        self.unsubscribe().await?;
+
+        Ok(())
+    }
+
+    async fn next_message(&mut self) -> Option<Result<(PeerAddress, HotstuffMessage), ConsensusGossipError>> {
         self.gossip
             .next_message()
             .await
             .map(|result| result.map_err(ConsensusGossipError::InvalidMessage))
     }
 
-    pub async fn subscribe(&mut self, epoch: Epoch) -> Result<(), ConsensusGossipError> {
-        /*
+    async fn subscribe(&mut self, epoch: Epoch) -> Result<(), ConsensusGossipError> {
         let committee_shard = self.epoch_manager.get_local_committee_info(epoch).await?;
+        let shard_group = committee_shard.shard_group();
+
         match self.is_subscribed {
-            Some(b) if b == committee_shard.shard_group() => {
+            Some(sg) if sg == shard_group => {
                 return Ok(());
             },
             Some(_) => {
@@ -66,37 +102,49 @@ impl ConsensusGossip<PeerAddress> {
             None => {},
         }
 
+        let topic = shard_group_to_topic(shard_group);
         self.gossip
-            .subscribe_topic(shard_group_to_topic(committee_shard.shard_group()))
+            .subscribe_topic(topic)
             .await?;
         self.is_subscribed = Some(committee_shard.shard_group());
-         */
+
         Ok(())
     }
 
-    pub async fn unsubscribe(&mut self) -> Result<(), ConsensusGossipError> {
-        /*
+    async fn unsubscribe(&mut self) -> Result<(), ConsensusGossipError> {
         if let Some(sg) = self.is_subscribed {
-            self.gossip.unsubscribe_topic(shard_group_to_topic(sg)).await?;
+            let topic = shard_group_to_topic(sg);
+            self.gossip.unsubscribe_topic(topic).await?;
             self.is_subscribed = None;
         }
-         */
+
         Ok(())
     }
 
-    pub async fn forward_to_local_replicas(&mut self, epoch: Epoch, msg: DanMessage) -> Result<(), ConsensusGossipError> {
-        /* 
-        let committee = self.epoch_manager.get_local_committee_info(epoch).await?;
+    async fn handle_new_message(
+        &mut self,
+        result: Result<(PeerAddress, HotstuffMessage), ConsensusGossipError>,
+    ) -> Result<(), ConsensusGossipError> {
+        let (from, msg) = result?;
 
-        let topic = shard_group_to_topic(committee.shard_group());
         debug!(
             target: LOG_TARGET,
-            "forward_to_local_replicas: topic: {}", topic,
+            "Received NEW consensus gossip message from {}: {:?}",
+            from,
+            msg
         );
 
-        let msg = proto::network::DanMessage::from(&msg);
-        self.gossip.publish_message(topic, msg).await?;
-        */
+        match msg {
+            HotstuffMessage::NewView(_msg) => {},
+            HotstuffMessage::Proposal(_msg) => {},
+            HotstuffMessage::ForeignProposal(msg) => {},
+            HotstuffMessage::Vote(_msg) => {},
+            HotstuffMessage::MissingTransactionsRequest(_msg) => {},
+            HotstuffMessage::MissingTransactionsResponse(_msg) => {},
+            HotstuffMessage::CatchUpSyncRequest(_msg) => {},
+            HotstuffMessage::SyncResponse(_msg) => {},
+        }
+
         Ok(())
     }
 
