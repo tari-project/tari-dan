f64bac794e9cac621a3d39ae2020fed8644e1542
Richard Bertok <bertok.richard.94@gmail.com>
2024-10-18 06:31:14 +0200

    feat(swarm): auto mining (#1174)
    Description
    ---
    Added auto mining feature to swarm daemon UI. It needs to be started by
    hand but afterwards it runs until stopped from UI.
    
    
    
    https://github.com/user-attachments/assets/90a00c45-3fef-47c8-a598-7e6ce7415363
    
    
    Motivation and Context
    ---
    To be able to simulate a real L1 network, we need to have constant
    mining on that, so we can see epoch changes and see how the
    network/consensus moves forward.
    
    How Has This Been Tested?
    ---
    
    
    https://github.com/user-attachments/assets/90a00c45-3fef-47c8-a598-7e6ce7415363
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify

--- applications/tari_swarm_daemon/src/webserver/rpc/miners.rs
+++ applications/tari_swarm_daemon/src/webserver/rpc/miners.rs
@@ -1,7 +1,12 @@
 //   Copyright 2024 The Tari Project
 //   SPDX-License-Identifier: BSD-3-Clause
 
+use std::time::Duration;
+
+use log::warn;
 use serde::{Deserialize, Serialize};
+use tari_shutdown::Shutdown;
+use tokio::select;
 
 use crate::webserver::context::HandlerContext;
 
@@ -16,4 +21,71 @@ pub struct MineResponse {}
 pub async fn mine(context: &HandlerContext, req: MineRequest) -> Result<MineResponse, anyhow::Error> {
     context.process_manager().mine_blocks(req.num_blocks).await?;
     Ok(MineResponse {})
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct StartMiningRequest {
+    interval_seconds: u64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct StartMiningResponse {}
+
+pub async fn start_mining(
+    context: &HandlerContext,
+    req: StartMiningRequest,
+) -> Result<StartMiningResponse, anyhow::Error> {
+    let shutdown = Shutdown::new();
+    context.start_mining(shutdown.clone()).await?;
+
+    let process_manager = context.process_manager().clone();
+    tokio::spawn(async move {
+        let shutdown_signal = shutdown.to_signal();
+        let interval = tokio::time::interval(Duration::from_secs(req.interval_seconds));
+        tokio::pin!(shutdown_signal);
+        tokio::pin!(interval);
+        loop {
+            select! {
+                _ = &mut shutdown_signal => {
+                    break;
+                }
+                _ = interval.tick() => {
+                    if let Err(error) = process_manager.mine_blocks(1).await {
+                        warn!("Failed to mine a block: {error:?}");
+                    }
+                }
+            }
+        }
+    });
+    Ok(StartMiningResponse {})
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct IsMiningRequest {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct IsMiningResponse {
+    result: bool,
+}
+
+pub async fn is_mining(context: &HandlerContext, _req: IsMiningRequest) -> Result<IsMiningResponse, anyhow::Error> {
+    Ok(IsMiningResponse {
+        result: context.is_mining().await,
+    })
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct StopMiningRequest {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct StopMiningResponse {
+    result: bool,
+}
+
+pub async fn stop_mining(
+    context: &HandlerContext,
+    _req: StopMiningRequest,
+) -> Result<StopMiningResponse, anyhow::Error> {
+    context.stop_mining().await;
+    Ok(StopMiningResponse { result: true })
 }
