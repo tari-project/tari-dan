39fc0f0ade7b4a0b81cab60e666ffa21fc99467c
ksrichard <bertok.richard.94@gmail.com>
2024-10-21 11:06:37 +0200

    removed unneeded todo

--- dan_layer/storage_sqlite/src/sqlite_transaction.rs
+++ dan_layer/storage_sqlite/src/sqlite_transaction.rs
@@ -29,7 +29,7 @@ use crate::error::SqliteStorageError;
 const _LOG_TARGET: &str = "tari::dan::storage::sqlite::transaction";
 
 pub struct SqliteTransaction<'a> {
-    connection: MutexGuard<'a, SqliteConnection>, // TODO: use Arc<Mutex<SqliteConnection>> instead
+    connection: MutexGuard<'a, SqliteConnection>,
     is_done: bool,
 }
 
